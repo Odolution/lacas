d2b2655b04dcff265e9252047984200ac9759de5
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2022-11-19 14:03:15 +0500

    Update main_view.py

--- cus_report/models/main_view.py
+++ cus_report/models/main_view.py
@@ -3,6 +3,10 @@
 
 from odoo import models, fields,api
 from odoo.exceptions import UserError
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+from odoo import http
+from odoo.http import request
 # import base64
 # import requests
 # import datetime
@@ -29,3 +33,21 @@ class inheritincompany(models.Model):
     _inherit = 'res.company'
 
     image = fields.Image(string='Bank Image')
+    
+class reportbutton(http.Controller):
+    @http.route('/attachment/download/<int:invoice_id>', methods=['POST', 'GET'], csrf=False, type='http', auth="user",
+                website=True)
+
+    def download_catalogue(self, **kw):
+
+        record_id = kw['invoice_id']
+        print(kw['invoice_id'])
+        """In this function we are calling the report template
+        of the corresponding product and
+        downloads the catalogue in pdf format"""
+
+        pdf, _ = request.env.ref('cus_report.report_admission_challan').sudo()._render_qweb_pdf(
+            [int(record_id)])
+        pdfhttpheaders = [('Content-Type', 'application/pdf'), ('Content-Length', len(pdf)),
+                          ('Content-Disposition', 'catalogue' + '.pdf;')]
+        return request.make_response(pdf, headers=pdfhttpheaders)
