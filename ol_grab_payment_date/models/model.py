a18f6115636bfb00bebef87259ebaf9ee09a8748
Usama Shahid <itisusamashahid@gmail.com>
2022-11-22 07:15:37 +0000

    added ol_grab_payment_date module
    added ol_grab_payment_date module

---
+++ ol_grab_payment_date/models/model.py
@@ -0,0 +1,17 @@
+
+from odoo import models, api, fields, _
+from odoo.exceptions import UserError
+
+
+
+
+
+class ext_invoice(models.Model):
+    _inherit = "account.move"
+    ol_payment_date = fields.Char(string='Payment Date')
+    @api.onchange('invoice_payments_widget')
+    def _onchange_invoice_payments_widget(self):
+        for rec in self:
+            if rec.invoice_payments_widget:
+                raise UserError(rec.invoice_payments_widget)
+    
