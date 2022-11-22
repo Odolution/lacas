ea8c9e32dd426428e2cf3aaece1752cca8af888b
Usama Shahid <itisusamashahid@gmail.com>
2022-11-22 09:04:55 +0000

    updates

--- ol_grab_payment_date/models/model.py
+++ ol_grab_payment_date/models/model.py
@@ -1,17 +1,23 @@
 
 from odoo import models, api, fields, _
 from odoo.exceptions import UserError
-
-
+import json
+import datetime
 
 
 
 class ext_invoice(models.Model):
     _inherit = "account.move"
-    ol_payment_date = fields.Char(string='Payment Date')
-    @api.onchange('invoice_payments_widget')
+    ol_payment_date = fields.Date(string='Payment Date')
+    @api.onchange('payment_state')
     def onchange_invoice_payments_widget(self):
+        print("onchange")
         for rec in self:
             if rec.invoice_payments_widget:
-                raise UserError(rec.invoice_payments_widget)
-    
+                date=""
+                try:
+                    date=str(json.loads(rec.invoice_payments_widget)["content"][-1]["date"])
+                except:
+                    pass
+                if date!="":
+                    rec.ol_payment_date=datetime.datetime.strptime(date,"%Y-%m-%d").date()
