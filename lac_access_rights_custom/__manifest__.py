704de56e7e2fc5c0cb81e45c3f0ed2432ed85118
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2022-11-26 23:31:00 +0500

    access_rights

---
+++ lac_access_rights_custom/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name": "Access Rights Custom",
+    "author": "Odolution",
+    "category": "Accounting",
+    "license": "OPL-1",
+    "version": "15.0.1",
+    "depends": [
+        'account_accountant'
+    ],
+
+    "data": [
+        'security/groups_user.xml',
+        'views/access_rights_menus.xml',
+
+    ],
+    "images": [],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+
+}
