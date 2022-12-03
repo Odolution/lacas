1a655ad74c1db90eb1fa9a3a6a8ebd725e4a403f
Usama Shahid <itisusamashahid@gmail.com>
2022-12-03 09:49:19 +0000

    added invoice sequence on  status change

--- challan_field/models/main_view.py
+++ challan_field/models/main_view.py
@@ -17,6 +17,37 @@ class account_fields(models.Model):
 
     reject_reason = fields.Selection(
         [('siblings', 'Siblings Discount'), ('Scholarship', 'Scholarship Discount'), ], 'Reason For Discount')
+    
+    @api.onchange('state')
+    def _onchange_appy_seq(self):
+        record = self
+
+        if record.state == 'posted':
+          if record.move_type == 'out_invoice':
+            record['name'] = 'Draft'
+            if 'Draft' in record.name:
+              # raise UserError(record)
+              if record.school_ids:
+                for school in record.school_ids:
+                  school_code = school.description
+              new_no = school_code + env['ir.sequence'].next_by_code('adm_challan')
+              record.sudo().write({
+                'name': new_no,
+              })
+              for rec in record.invoice_line_ids:
+                rec['name'] = new_no
+          if record.move_type == 'out_refund':
+            record['name'] = 'Draft'
+            if 'Draft' in record.name:
+              # raise UserError(record)
+              if record.x_school_id_cred:
+                new_no = record.x_school_id_cred.description + env['ir.sequence'].next_by_code('security')
+                record.sudo().write({
+                  'name': new_no,
+                })
+                for rec in record.invoice_line_ids:
+                  rec['name'] = new_no
+              # raise UserError(new_no)
 
 
 # class school_panel_field(models.Model):
