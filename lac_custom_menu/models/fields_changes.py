a01be3af6bc805617464bda6eac1f94c37af5e5e
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2022-12-07 22:10:09 +0500

    compute_udid

--- lac_custom_menu/models/fields_changes.py
+++ lac_custom_menu/models/fields_changes.py
@@ -3,7 +3,14 @@ from odoo.exceptions import UserError
 
 class field_changes_custom_update(models.Model):
     _inherit = 'account.move'
-    udid_new_lv = fields.Char(string="UDID NEW")
+    udid_new_lv = fields.Char(compute='_compute_udid_student',string="UDID NEW")
 
     
-    
+    def _compute_udid_student(self):
+        # if self.move_type == "out_refund":
+        for rec in self:
+            if rec.tuition_plan_ids:
+                for stu_rec in rec.tuition_plan_ids:
+                    rec.udid_new_lv=stu_rec.student_id.facts_udid
+            else:
+                rec.udid_new_lv=""
