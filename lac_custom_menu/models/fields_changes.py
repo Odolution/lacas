d6d703752e5022ad718f95cb84ca42c61a870743
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2022-12-08 01:37:18 +0500

    update_facts_id_lv

--- lac_custom_menu/models/fields_changes.py
+++ lac_custom_menu/models/fields_changes.py
@@ -4,6 +4,7 @@ from odoo.exceptions import UserError
 class field_changes_custom_update(models.Model):
     _inherit = 'account.move'
     udid_new_lv = fields.Char(compute='_compute_udid_student',string="UDID NEW")
+    facts_id_new_lv = fields.Char(compute='_compute_facts_id_student',string="Facts Id NEW")
 
     
     def _compute_udid_student(self):
@@ -21,3 +22,14 @@ class field_changes_custom_update(models.Model):
             #         rec.udid_new_lv=stu_rec.student_id.facts_udid
             # else:
             #     rec.udid_new_lv=""
+
+    def _compute_facts_id_student(self):
+        # if self.move_type == "out_refund":
+        for rec in self:
+            if rec.student_ids:
+                for stu_rec in rec.student_ids:
+                    rec.facts_id_new_lv=stu_rec.facts_id
+            else:
+                rec.facts_id_new_lv=""
+
+            
