f826a9523d27f8a57633daee4ef9006216c15d0a
Nadia Faqir Muhammad <46475381+NadiaFM@users.noreply.github.com>
2022-12-16 17:01:05 +0500

    Update model.py

--- ol_lacas_custom_trees/models/model.py
+++ ol_lacas_custom_trees/models/model.py
@@ -37,6 +37,8 @@ class ext(models.Model):
     security_amount=fields.Char(string="Security Amount",compute="_onchange_security_amount_data")
     bill_amount=fields.Char(string="Bill Amount",compute="_onchange_bill_amount_data")
     std_factsid=fields.Integer(string="Facts ID",compute="_onchange_facts_id_data")
+    std_payment_date=fields.Date(string='Payment Date',compute="_onchange_payment_date_data")
+
 
     
 
@@ -476,6 +478,27 @@ class ext(models.Model):
         self.bill_amount=''
         for rec in adm_journals:
             rec['bill_amount']=rec.amount_total
+ 
+
+    @api.onchange('payment_state')
+    def _onchange_payment_date_data(self):
+        self._get_payment_date_field
+        
+    def _get_payment_date_field(self):
+        adm_journal=self.env['account.journal'].search([('code','=','ADM')])
+        adm_journals=self.env['account.move'].search([('journal_id','=',adm_journal.id)])
+ 
+        for rec in adm_journals:
+            if rec.payment_state == "paid":
+                date=""
+                try:
+                    date=str(json.loads(rec.invoice_payments_widget)["content"][-1]["date"])
+                except:
+                    pass
+                if date!="":
+                    rec.std_payment_date=datetime.datetime.strptime(date,"%Y-%m-%d").date()
+                else:
+                    rec.std_payment_date=False
    
 
             
