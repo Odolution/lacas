41aa35abea2f0be9ccf044fa10db73873afbd60e
Anique <68085660+AniqueKhan@users.noreply.github.com>
2022-12-21 12:10:38 +0500

    bug fix attempt 3
    bug fix attempt 3

--- user_wise_invoices/models/account.py
+++ user_wise_invoices/models/account.py
@@ -6,10 +6,17 @@ class UserWiseInvoiceAccount(models.Model):
 
     # student_id = fields.Integer(string="School")
     
+    program_id = fields.Many2one('school.program',string='Program ID')
+
+    @api.onchange('program_ids')
+    def calc_program_id(self):
+        if self.program_ids: 
+            self.program_id = self.program_ids[0]
+    
     def get_invoices_action(self):
         action = self.env.ref('account.action_move_out_invoice_type').read()[0]
         user=self.env["res.users"].search([("id",'=',self.env.uid)])
-        domain = [('program_ids','in',[user.user_program_id)]]
+        domain = [('program_id','in',[user.user_program_id)]]
         action['domain'] = domain
         return action
             
