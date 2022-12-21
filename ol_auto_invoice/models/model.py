fb5e87b685aa6d3c5037ed3d31b5590e6fba90e1
Usama Shahid <itisusamashahid@gmail.com>
2022-12-21 14:53:46 +0000

    spelling fix
    spelling fix

--- ol_auto_invoice/models/model.py
+++ ol_auto_invoice/models/model.py
@@ -7,7 +7,7 @@ class ext(models.Model):
         todaydate=datetime.now().date()
         enrolement_status = self.env['school.enrollment.status'].search([('name','=',"Enrolled")])
         enrollment_ids=[i.id for i  in enrolement_status]
-        students= self.env['school.student'].search([('enrollement_status_ids','in',enrollment_ids)])
+        students= self.env['school.student'].search([('enrollment_status_ids','in',enrollment_ids)])
         journals = self.env['account.journal'].search([('name','=',"Monthly Bills")])
         journal_ids=[i.id for i  in journals]
         tuition_plans=self.env["tuition.plan"].search([('student_id','in',students.ids),('journal_id','in',journal_ids)])
