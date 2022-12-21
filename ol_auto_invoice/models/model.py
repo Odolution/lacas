8ef7db7ee70df7438a30b3dddfdcfba3e4da3e96
Usama Shahid <itisusamashahid@gmail.com>
2022-12-21 14:45:29 +0000

    bug fixes for many2many issues
    bug fixes for many2many issues

--- ol_auto_invoice/models/model.py
+++ ol_auto_invoice/models/model.py
@@ -6,8 +6,12 @@ class ext(models.Model):
     _inherit="account.move"
     def create_auto_invoices(self):
         todaydate=datetime.now().date()
-        students= self.env['school.student'].search([('enrolement_status_ids.name','in',['Enrolled'])])
-        tuition_plans=self.env["tuition.plan"].search([('student_id','in',students.ids),('journal_id.name','in',["Monthly Bills"])])
+        enrolement_status = self.env['school.enrollment.status'].search([('name','=',"Enrolled")])
+        enrollment_ids=[i.id for i  in enrolement_status]
+        students= self.env['school.student'].search([('enrolement_status_ids','in',enrollment_ids)])
+        journals = self.env['account.journal'].search([('name','=',"Monthly Bills")])
+        journal_ids=[i.id for i  in journals]
+        tuition_plans=self.env["tuition.plan"].search([('student_id','in',students.ids),('journal_id','in',journal_ids)])
         installments=self.env["tuition.installment"].search([("tuition_plan_id","in",tuition_plans.ids),("x_inv_date",'=',todaydate)])
         for installment in installments:
             tuition_plan = installment.tuition_plan_id
