74d7e11ec02a0b4e98982ce716875cb397e01609
Usama Shahid <itisusamashahid@gmail.com>
2022-12-22 13:06:24 +0000

    bug fix
    bug fix

--- ol_auto_invoice/models/model.py
+++ ol_auto_invoice/models/model.py
@@ -10,7 +10,7 @@ class ext(models.Model):
         students= self.env['school.student'].search([('enrollment_status_ids','in',enrollment_ids)])
         journals = self.env['account.journal'].search([('name','=',"Monthly Bills")])
         journal_ids=[i.id for i  in journals]
-        tuition_plans=self.env["tuition.plan"].search([('student_id','in',students.ids),('journal_id','in',journal_ids)])
+        tuition_plans=self.env["tuition.plan"].search([('student_id','in',students.ids),('journal_id','in',journal_ids),('state','=','posted')])
         
         # installments=env["tuition.installment"].search([("tuition_plan_id","in",tuition_plans.ids),("x_inv_date",'=',todaydate)])
 
