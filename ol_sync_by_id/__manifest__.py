1fc5ae3826a7ae270526085367a4e704c8ead60a
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2022-12-24 18:19:51 +0500

    ol_sync_by_id

---
+++ ol_sync_by_id/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name": "Sync Student By ID",
+
+    "author": "Odolution",
+    "category": "Other",
+
+    "license": "OPL-1",
+
+    "version": "14.0.1",
+
+    "depends": [
+        'contacts'
+    ],
+
+    "data": [
+        'security/ir.model.access.csv',
+        'views/main_view.xml',
+    ],
+    "images": [ ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+}
