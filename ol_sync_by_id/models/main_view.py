6783f7a8cd764148bb0de10a59ca88d730afe249
hunain iqbal <91910170+hunain12iqbal@users.noreply.github.com>
2022-12-26 13:08:50 +0500

    test
    test

--- ol_sync_by_id/models/main_view.py
+++ ol_sync_by_id/models/main_view.py
@@ -36,12 +36,28 @@ class RespartnerInherit(models.Model):
                 enroled = self.env['school.enrollment.status'].search([])
                 for k in enroled:
                     if enrol == k.name:
-                        if enrol == "Graduate" or enrol == "Admissions" or enrol == "Pre-Enrolled":
+                        if enrol == "Withdrawn" or enrol == "Graduate":
                             rec['grade_level_ids'] = False
                             rec['enrollment_status_ids'] = k
                             for ngrade_rec in rec.enrollment_state_ids:
+                                ngrade_rec.grade_level_id = False
                                 ngrade_rec.next_grade_level_id = False
+                                ngrade_rec.next_enrollment_status_id = False
                             
+                        if  enrol == "Admissions" or enrol == "Pre-Enrolled":
+                            rec['grade_level_ids'] = False
+                            rec['enrollment_status_ids'] = k
+                            for ngrade_rec in rec.enrollment_state_ids:
+                                grade = self.env['school.grade.level'].search([])
+                                Next_Enrolled = data['Next_Enrolled']
+                                if Next_Enrolled == k.name:
+                                    nxt = data['nxt_grade']
+                                    for nxtgrd in grade:
+                                        if nxt == nxtgrd.name: 
+                                            ngrade_rec.grade_level_id = False
+                                            ngrade_rec.next_grade_level_id = nxt.id
+                                            ngrade_rec.next_enrollment_status_id = k.id
+
                         else:
                             rec['enrollment_status_ids'] = k
                             if  'grade_level' in data:
@@ -59,7 +75,8 @@ class RespartnerInherit(models.Model):
                                             for ngrade_rec in rec.enrollment_state_ids:
                                                 if j.name == nxt_grade:
                                                     ngrade_rec.next_grade_level_id = j.id
-                                                    ngrade_rec.grade_level_id = j.id 
+                                                    ngrade_rec.grade_level_id = j.id
+                                                    ngrade_rec.next_enrollment_status_id = k.id
 
                                     
                                         
