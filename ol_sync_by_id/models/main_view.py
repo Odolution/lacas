4f34db5677853a077518b3dbfa6bcecd9afd9540
hunain iqbal <91910170+hunain12iqbal@users.noreply.github.com>
2022-12-26 16:48:21 +0500

    test
    test

--- ol_sync_by_id/models/main_view.py
+++ ol_sync_by_id/models/main_view.py
@@ -42,45 +42,26 @@ class RespartnerInherit(models.Model):
                                 next_line.next_grade_level_id = False
                                 next_line.next_enrollment_status_id = False
                        
+                for k in enroled:
                     if str(enrol) == "Admissions":
                         if str(enrol) == str(k.name):
-                            
                             rec['enrollment_status_ids'] = k
                             rec['grade_level_ids'] = False
-#                             nxt_status = self.env['school.enrollment.status'].search([])
-#                             for next_line in rec.enrollment_state_ids:
-#                                 Next_Enrolled = data['Next_Enrolled']
-#                                 for f in nxt_status:
-#                                     if str(Next_Enrolled) == str(f.name):
-#                                         nxt = data['nxt_grade']
-#                                         for nxtgrd in gradelvl:
-#                                             if str(nxt) == str(nxtgrd.name):
-#                                                 next_line.grade_level_id = False
-#                                                 next_line.next_grade_level_id = nxtgrd.id
-#                                                 next_line.next_enrollment_status_id = f.id
+                            nxt_status = self.env['school.enrollment.status'].search([])
+                            for next_line in rec.enrollment_state_ids:
+                                Next_Enrolled = data['Next_Enrolled']
+                                for f in nxt_status:
+                                    if str(Next_Enrolled) == str(f.name):
+                                        nxt = data['nxt_grade']
+                                        for nxtgrd in gradelvl:
+                                            if str(nxt) == str(nxtgrd.name):
+                                                next_line.grade_level_id = False
+                                                next_line.next_grade_level_id = nxtgrd.id
+                                                next_line.next_enrollment_status_id = f.id
 
 
                                                 
-                    if str(enrol) == "Enrolled":
-                        if str(enrol) == str(k.name):
-                            rec['enrollment_status_ids'] = k
-                            if  'grade_level' in data:
-                                if data['grade_level']:
-                                    grade_name = data['grade_level']
-                                    for j in gradelvl:
-                                        if j.name == grade_name:
-                                            rec['grade_level_ids'] = j
-#                                             for ngrade_rec in rec.enrollment_state_ids:
-#                                                 ngrade_rec.grade_level_id = j.id
-#                                                 ngrade_rec.next_enrollment_status_id = k.id
-#                                         if data['nxt_grade']:
-#                                             nxt_grade = data['nxt_grade']
-#                                             for ngrade_rec in rec.enrollment_state_ids:
-#                                                 if j.name == nxt_grade:
-#                                                     ngrade_rec.next_grade_level_id = j.id
-#                                                 if data['Next_Enrolled'] == k.name:
-#                                                     ngrade_rec.next_enrollment_status_id = k.id
-
+                   
                                         
                                                 
                                                 
