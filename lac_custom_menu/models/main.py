530ea8b692599572ce6b18cfa2cc23eb478747cc
Nadia Faqir Muhammad <46475381+NadiaFM@users.noreply.github.com>
2022-12-30 16:09:16 +0500

    Update main.py

--- lac_custom_menu/models/main.py
+++ lac_custom_menu/models/main.py
@@ -7,7 +7,7 @@ class field_changes_custom_update(models.Model):
     facts_id_new_lv = fields.Char(string="Facts Id")
     adm_amount=fields.Char(string="Admission Amount")
     security_amount=fields.Char(string="Security Amount")
-    std_dob=fields.Char(compute="_onchange_dob_data")
+    std_dob=fields.Char(string="Date Of Birth")
 
     @api.onchange('x_student_id_cred',"student_ids")
     def _studentsbill_onchange(self):
@@ -19,7 +19,7 @@ class field_changes_custom_update(models.Model):
 
         if self.student_ids:
             self.udid_new_lv=self.student_ids.facts_udid
-            self.facts_id_new_lv=self.student_ids.facts_udid
+            self.facts_id_new_lv=self.student_ids.facts_id
             self.adm_amount=self.student_ids.school_ids.name
             self.std_dob=self.student_ids.date_of_birth
             self.security_amount=self.invoice_date_due
