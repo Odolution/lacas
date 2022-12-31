511a72c89b3286e7bf9bf7ea9edea6cb5a084312
Nadia Faqir Muhammad <46475381+NadiaFM@users.noreply.github.com>
2022-12-31 13:02:15 +0500

    Update model.py

--- ol_lacas_custom_trees/models/model.py
+++ ol_lacas_custom_trees/models/model.py
@@ -179,18 +179,15 @@ class ext(models.Model):
                 var=str(json.loads(self.invoice_payments_widget)["content"][-1]["date"])
                 self.std_payment_date=var
                 
+
             if self.student_ids.school_ids:
-                if len(self.student_ids.school_ids)>1:
-                    if self.student_ids.enrollment_history_ids:
-                        enroll_history=self.student_ids.enrollment_history_ids
-                        lst=[]
-                        for lines in enroll_history:
-                            lst.append(lines.program_id.name)
-                        self.campus=lst[0]
-                else:
-                    self.campus=self.student_ids.school_ids.name
-             
-            
+                if self.student_ids.enrollment_history_ids:
+                    enroll_history=self.student_ids.enrollment_history_ids
+                    lst=[]
+                    for lines in enroll_history:
+                        lst.append(lines.program_id.name)
+                    self.campus=lst[0]
+               
             
             
                 
