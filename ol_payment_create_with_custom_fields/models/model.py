f928bb034542fb34df27b9d67584eb89860f97ce
Nadia Faqir Muhammad <46475381+NadiaFM@users.noreply.github.com>
2023-01-02 13:29:49 +0500

    Update model.py

--- ol_payment_create_with_custom_fields/models/model.py
+++ ol_payment_create_with_custom_fields/models/model.py
@@ -9,24 +9,28 @@ class extwiz(models.TransientModel):
     _inherit = "account.payment.register"
     ol_check_in_favor_of = fields.Char('Check in Favor Of')
     
+    
     @api.onchange('communication')
     def _compute_check_in_favor_of(self):
-        for lines in self.line_ids:
+        for lines in self.lines_id:
             if lines.student_id.name:
                 self.ol_check_in_favor_of = lines.student_id.name
             else:
-                inv=self.env['account.move'].search(["name",'=',self.communication])
-                if inv:
-                    for lines in inv.invoice_line_ids:
-                        if lines.student_id:
-                            student=lines.student_id
-                        if student.relationship_ids:
-                            relation=student.relationship_ids
-                            for lines in relation:
-                                if lines.relationship_type_id.name=="Father":
-                                    self.ol_check_in_favor_of =lines.individual_id.name
-                else:
-                    self.ol_check_in_favor_of = "None"
+                invoice=self.env['account.move'].search([])
+                for rec in self:
+                    for inv in invoice:
+                        if rec.communication==inv.name:
+                            for lines in inv.invoice_line_ids:
+                                if lines.student_id:
+                                    student=lines.student_id
+                                if student.relationship_ids:
+                                    relation=student.relationship_ids
+                                    for lines in relation:
+                                        if lines.relationship_type_id.name=="Father":
+                                            self.ol_check_in_favor_of =lines.individual_id.name
+                        else:
+                            self.ol_check_in_favor_of = "None"
+
 
                     
                                   
