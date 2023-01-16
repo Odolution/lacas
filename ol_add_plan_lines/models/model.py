df28d80d8e258bfcd7d430b01c26d047a3ecc2a5
Usama Shahid <itisusamashahid@gmail.com>
2023-01-16 13:30:46 +0000

    test
    test

--- ol_add_plan_lines/models/model.py
+++ ol_add_plan_lines/models/model.py
@@ -4,6 +4,8 @@ from odoo.exceptions import UserError
 import json
 import datetime
 
+
+
 class add_plan_line_wiz(models.TransientModel):
     _name='tuition.add_plan_line_wiz'
     plan_ids = fields.Many2many('tuition.plan', string='tuition_plan')
