8398efd85e596f7f29b4641c15c378d95079bb84
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-01-23 03:21:20 +0500

    update_reversal_udid_onchng

--- custom_withdrawals/models/lacas_model.py
+++ custom_withdrawals/models/lacas_model.py
@@ -21,6 +21,12 @@ class academics_tab(models.Model):
     refund_receive = fields.Char(
         compute='_compute_refund_receive', string="Receivable/Refundable")
     
+
+    @api.onchange('x_student_id_cred')
+    def _student_reversal_onchange(self):
+      self.udid_cred_custom=""
+      if self.x_student_id_cred:
+        self.udid_cred_custom=self.x_student_id_cred.facts_udid
     
     def _compute_notice_fee(self):
         # if self.move_type == "out_refund":
