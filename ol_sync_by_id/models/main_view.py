c85e32a5ee03d2b56e19569d3c60ee45d8cd1ff4
hunain12iqbal <hunnayyt@gmail.com>
2023-01-27 15:18:39 +0500

    Update main_view.py

--- ol_sync_by_id/models/main_view.py
+++ ol_sync_by_id/models/main_view.py
@@ -14,7 +14,7 @@ class RespartnerInherit(models.Model):
     def sync_wd_facts(self,std):
         fact_obj = Fact_Api()
         fact_data = fact_obj.main(std)
-        raise UserError(str(fact_data))
+        raise UserError(str("hunain"))
         
         for rec in std:
             
