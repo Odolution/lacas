07e3f9a3401319f550bcbbca8ee93d2de63b406a
Nadia Faqir Muhammad <46475381+NadiaFM@users.noreply.github.com>
2023-02-04 15:32:58 +0500

    fix

--- cus_report/models/main_view.py
+++ cus_report/models/main_view.py
@@ -73,17 +73,19 @@ class inheritinvoices(models.Model):
             rec.unpaid_inv_ids=self.env['account.move'].search([("move_type","=","out_invoice"),("partner_id","=",rec.partner_id.id),("payment_state","=","not_paid")])
            
     def _compute_remaining_days(self):
-        self.due_day=0
-        self.due_day_text=""
-        if self.invoice_date_due:
-            d1 = datetime.strptime(str(datetime.now().date()), "%Y-%m-%d")
-            d2 = datetime.strptime(str(self.invoice_date_due), "%Y-%m-%d")
-            delta = d1 - d2
-            self["due_day"]=delta.days
-            if delta.days>0:
-                self["due_day_text"]="Outstanding"
-            else:
-                self["due_day_text"]=""
+       
+        for rec in self:
+            rec.due_day=0
+            rec.due_day_text=""
+            if rec.invoice_date_due:
+                d1 = datetime.strptime(str(datetime.now().date()), "%Y-%m-%d")
+                d2 = datetime.strptime(str(rec.invoice_date_due), "%Y-%m-%d")
+                delta = d1 - d2
+                rec["due_day"]=delta.days
+                if delta.days>0:
+                    rec["due_day_text"]="Outstanding"
+                else:
+                    rec["due_day_text"]=""
                 
                
 
