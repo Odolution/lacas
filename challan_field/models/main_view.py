664b9b189171ddd109fc69b870756c658c2a3f09
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-02-07 13:16:29 +0500

    update_Seq

--- challan_field/models/main_view.py
+++ challan_field/models/main_view.py
@@ -101,7 +101,11 @@ class account_fields(models.Model):
                   if record.x_school_id_cred:
                     for school in record.x_school_id_cred:
                       school_code = school.description
-                  new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
+                 # new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
+                  if record.journal_id.id == 119:
+                    new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
+                  if record.journal_id.id == 125:
+                    new_no = school_code + record.env['ir.sequence'].next_by_code('monthly_bills')
                   record.name = new_no
                     
                   for rec in record.line_ids:
