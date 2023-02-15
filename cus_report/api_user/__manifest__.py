dd627cf7f4ccef5f5bc503c97d506a50df5be2fa
Nadia Faqir Muhammad <46475381+NadiaFM@users.noreply.github.com>
2023-02-15 15:21:37 +0500

    api user module

---
+++ cus_report/api_user/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Token Authentication  ',
+
+    "author": "Nadia | Odolution",
+    'version': '0.1',
+    'category': '',
+    'sequence': -100,
+    'summary': '.',
+    'description': "",
+    'website': '',
+    'images': [
+    ],
+    'depends': ["account",
+    ],
+    'data': [
+        
+        "views/api.xml",
+    
+
+        
+
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [
+    ],
+    'license': 'LGPL-3',
+}
