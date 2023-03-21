56b1e6538345c1902d1bea8b2be3d375154977e4
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-03-21 18:00:06 +0500

    update

--- ol_lacas_custom_trees/models/model.py
+++ ol_lacas_custom_trees/models/model.py
@@ -100,7 +100,7 @@ class ext(models.Model):
         return action  
     
     def get_bimonthlyBill_action(self):
-        action = self.env.ref('ol_lacas_custom_trees.act_account_move_bimonthlyBill').read()[0]
+        action = self.env.ref('ol_lacas_custom_trees.act_account_move_bimonthly').read()[0]
         journals=self.env["account.journal"].search([("name","=","Bi Monthly")])
         domain = [('journal_id','in',[i.id for i in journals])]
         action['domain'] = domain
