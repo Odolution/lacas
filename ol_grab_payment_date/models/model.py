644dd88ae32350ba25f85fcd1653976d041efe3c
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-03-22 18:47:16 +0500

    update_payment_for_0_bill

--- ol_grab_payment_date/models/model.py
+++ ol_grab_payment_date/models/model.py
@@ -14,12 +14,17 @@ class ext_invoice(models.Model):
         print("onchange")
         for rec in self:
             if rec.payment_state == "paid":
-                date=""
-                try:
-                    date=str(json.loads(rec.invoice_payments_widget)["content"][-1]["date"])
-                except:
-                    pass
-                if date!="":
-                    rec.ol_payment_date=datetime.datetime.strptime(date,"%Y-%m-%d").date()
-                else:
-                    rec.ol_payment_date=False
+                if self.amount_residual>0:
+
+                    date=""
+                    try:
+                        date=str(json.loads(rec.invoice_payments_widget)["content"][-1]["date"])
+                    except:
+                        pass
+                    if date!="":
+                        rec.ol_payment_date=datetime.datetime.strptime(date,"%Y-%m-%d").date()
+                    else:
+                        rec.ol_payment_date=False
+                if self.amount_residual==0:
+                    self.ol_payment_date=self.x_studio_payment_for_zero_bill
+
