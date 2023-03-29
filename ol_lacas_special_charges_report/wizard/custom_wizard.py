5554a9b700255b040150576438c0b4f4abbe3571
Nadia Faqir Muhammad <46475381+NadiaFM@users.noreply.github.com>
2023-03-29 12:24:17 +0500

    .

--- ol_lacas_special_charges_report/wizard/custom_wizard.py
+++ ol_lacas_special_charges_report/wizard/custom_wizard.py
@@ -278,11 +278,11 @@ class SiblingsReportWizard(models.TransientModel):
         if xlwt:
 
 
-            file_name='Specilaization Charges Report'
+            filename='Specilaization Charges Report'
             if self.all_batch==True:
                 filename = 'All Batch Specilaization Charges Report '
             elif self.one_batch:
-                file_name=dict(self._fields['one_batch'].selection).get(self.one_batch)+'Specilaization Charges Report'
+                filename=dict(self._fields['one_batch'].selection).get(self.one_batch)+'Specilaization Charges Report'
          
                
             # One sheet by partner
