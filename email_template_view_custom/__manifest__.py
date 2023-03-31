991280996d2eb31865e58821cf32d6751cba242b
Nadia Faqir Muhammad <46475381+NadiaFM@users.noreply.github.com>
2023-03-31 15:18:51 +0500

    ..

---
+++ email_template_view_custom/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name": "Email Template View Customization",
+
+    "author": "odolution",
+    "developer": "Nadia fm",
+    "category": "",
+
+    "license": "OPL-1",
+
+    "version": "15.0.1",
+
+    "depends": ['mail',
+       
+    ],
+
+    "data": [
+        'views/customize_template.xml',
+    ],
+
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+
+
+}
