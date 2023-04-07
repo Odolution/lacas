f23f1fad7a3e74bfe024230b210c315ed17ea939
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-04-07 10:10:39 +0500

    net_fee

--- custom_withdrawals/models/lacas_model.py
+++ custom_withdrawals/models/lacas_model.py
@@ -50,7 +50,7 @@ class academics_tab(models.Model):
             if rec.invoice_line_ids:
                 for cred in rec.invoice_line_ids:
                     rec.amount_total_withdrawal = abs(
-                        rec.notice_fee_withdrawal-cred.price_subtotal)
+                        cred.notice_fee_withdrawal-cred.price_subtotal)
             else:
                 rec.amount_total_withdrawal = 0
 
