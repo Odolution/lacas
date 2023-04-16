6a699af946f1a0e7658889b1cb570a758acf5696
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-04-17 04:16:00 +0500

    update fix

--- ol_lacas_custom_trees/models/model.py
+++ ol_lacas_custom_trees/models/model.py
@@ -304,7 +304,7 @@ class ext(models.Model):
         self.security_amount=""
         self.bill_amount=""
         self.net_amount=""
-        self.std_factsid=""
+        #self.std_factsid=""
         self.std_payment_date=""
         self.section_name=""
         # self.art=0
@@ -355,7 +355,7 @@ class ext(models.Model):
             self.std_batch=self.x_studio_batch.x_name
             # self.std_dob=self.student_ids.date_of_birth
             #self.std_fathername=self.partner_id.name
-            self.std_factsid=self.student_ids.facts_id
+            #self.std_factsid=self.student_ids.facts_id
             self.std_contactno=self.partner_id.mobile
             self.bill_amount=int(self.amount_total)
 
