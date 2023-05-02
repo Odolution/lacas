9d6fc1e8fd573aa9f136f9189fceea1d01f6be3a
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-05-02 14:14:27 +0500

    update

--- ol_late_fee_policy/models/model.py
+++ ol_late_fee_policy/models/model.py
@@ -138,7 +138,7 @@ class ext_invoice(models.Model):
         return max        
 
     def apply_late_fee_policy(self):
-        raise UserError(self._compute_amount())
+        #raise UserError(self._compute_amount())
 
         
         for invoice in self:
