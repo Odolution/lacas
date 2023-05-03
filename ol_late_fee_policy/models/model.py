0720b158e07757c47ee8ba49b19aa92d2cdbf020
Nadia Faqir Muhammad <46475381+NadiaFM@users.noreply.github.com>
2023-05-03 12:59:29 +0500

    ,

--- ol_late_fee_policy/models/model.py
+++ ol_late_fee_policy/models/model.py
@@ -120,7 +120,8 @@ class ext_invoice(models.Model):
             charge=policy[key]
             numberOfdays=days[key]
             if key is "remaining":##If number of days are going beyond the defined slab, then add the given amount every 10 days. 
-                charges[key]=(charge*int(numberOfdays/dif))
+                charges[key]=charge+(charge*int(numberOfdays/dif))
+                raise UserError(charges["remaining"])
                 # if 30<dif<40:
                 #     charges[key]=charge+charge
                 # else:
