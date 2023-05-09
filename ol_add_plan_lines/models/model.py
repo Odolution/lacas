3c3b92cc1036ac39fb87e89862bd5bfd11cedae3
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-05-09 18:31:25 +0500

    update

--- ol_add_plan_lines/models/model.py
+++ ol_add_plan_lines/models/model.py
@@ -79,7 +79,7 @@ class add_plan_line_wiz(models.TransientModel):
                             tuition_plan=self.env['tuition.plan'].search([('id','=',plan.id)])
                             if self.unit_price==0:
                                 tuition_plan["x_studio_have_discount_1"]=0
-                            if self.unit_price<0:
+                            if self.unit_price>0:
                                 tuition_plan["x_studio_have_discount_1"]=1
                         
                         linedata={
