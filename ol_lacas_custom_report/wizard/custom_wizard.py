408d7a998a58e240da6ffa2cc29a6face2b5cbe3
Nadia Faqir Muhammad <46475381+NadiaFM@users.noreply.github.com>
2023-05-19 09:21:42 +0500

    .

--- ol_lacas_custom_report/wizard/custom_wizard.py
+++ ol_lacas_custom_report/wizard/custom_wizard.py
@@ -484,6 +484,7 @@ class ReceivablesReportWizard(models.TransientModel):
             
         
             temp_list_2.append(custom_dataa)
+        raise UserError(temp_list_2)
         
 
         
