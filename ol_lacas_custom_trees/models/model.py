b1b47119927912d88da4a085522b6216ecc19cf6
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-05-20 01:48:25 +0500

    update

--- ol_lacas_custom_trees/models/model.py
+++ ol_lacas_custom_trees/models/model.py
@@ -149,183 +149,185 @@ class ext(models.Model):
 
     @api.onchange('invoice_line_ids')
     def _invoice_lines_onchange(self):
-        if self.invoice_line_ids: 
+        for rec_inv in self:
 
-            self.tuition=0
-            self.club=0
-            self.computer=0
-            self.library=0
-            self.utility=0
-            self.art=0
-            self.biology=0
-            self.chemistry=0
-            self.physics=0
-            self.computing=0
-            self.classphoto=0
-            self.collegemagazine=0
-            self.dc=0
-            self.ec=0
-            self.farewell=0
-            self.gatepass=0
-            self.idcard=0
-            self.idcardfine=0
-            self.latecoming=0
-            self.latefee=0
-            self.libfine=0
-            self.mnf=0
-            self.mobfine=0
-            self.news=0
-            self.paragon=0
-            self.books=0
-            self.pcopy=0
-            self.photo=0
-            self.scarf=0
-            self.sportd=0
-            self.stationary=0
-            self.welcome=0
-            self.workbook=0
-            self.uniform=0
-            self.continuation=0
-            self.adm_amount=""
+            if rec_inv.invoice_line_ids: 
+
+                rec_inv.tuition=0
+                rec_inv.club=0
+                rec_inv.computer=0
+                rec_inv.library=0
+                rec_inv.utility=0
+                rec_inv.art=0
+                rec_inv.biology=0
+                rec_inv.chemistry=0
+                rec_inv.physics=0
+                rec_inv.computing=0
+                rec_inv.classphoto=0
+                rec_inv.collegemagazine=0
+                rec_inv.dc=0
+                rec_inv.ec=0
+                rec_inv.farewell=0
+                rec_inv.gatepass=0
+                rec_inv.idcard=0
+                rec_inv.idcardfine=0
+                rec_inv.latecoming=0
+                rec_inv.latefee=0
+                rec_inv.libfine=0
+                rec_inv.mnf=0
+                rec_inv.mobfine=0
+                rec_inv.news=0
+                rec_inv.paragon=0
+                rec_inv.books=0
+                rec_inv.pcopy=0
+                rec_inv.photo=0
+                rec_inv.scarf=0
+                rec_inv.sportd=0
+                rec_inv.stationary=0
+                rec_inv.welcome=0
+                rec_inv.workbook=0
+                rec_inv.uniform=0
+                rec_inv.continuation=0
+                rec_inv.adm_amount=""
 
 
-            for line in self.invoice_line_ids:
-                if 'Tuition Fee' in line.product_id.name:
-                        self.tuition=line.price_subtotal
-                elif 'Club' in line.product_id.name:
-                    self.club=line.price_subtotal
-                elif 'Computer' in line.product_id.name:
-                    self.computer=line.price_subtotal
-                elif 'Library' in line.product_id.name:
-                    self.library=line.price_subtotal
-                elif 'Utility' in line.product_id.name:
-                    self.utility=line.price_subtotal
-                elif 'Admission' in line.product_id.name:
-                    adm_amount=int(line.price_subtotal)
-                    self.adm_amount=str(adm_amount)
-                elif 'Security' in line.product_id.name:
-                    security_amount=int(line.price_subtotal)
-                    self.security_amount=str(security_amount)
+                for line in rec_inv.invoice_line_ids:
+                    if 'Tuition Fee' in line.product_id.name:
+                            rec_inv.tuition=line.price_subtotal
+                    elif 'Club' in line.product_id.name:
+                        rec_inv.club=line.price_subtotal
+                    elif 'Computer' in line.product_id.name:
+                        rec_inv.computer=line.price_subtotal
+                    elif 'Library' in line.product_id.name:
+                        rec_inv.library=line.price_subtotal
+                    elif 'Utility' in line.product_id.name:
+                        rec_inv.utility=line.price_subtotal
+                    elif 'Admission' in line.product_id.name:
+                        adm_amount=int(line.price_subtotal)
+                        rec_inv.adm_amount=str(adm_amount)
+                    elif 'Security' in line.product_id.name:
+                        security_amount=int(line.price_subtotal)
+                        rec_inv.security_amount=str(security_amount)
 
-                elif 'Class Photo' in line.product_id.name:
-                    self.classphoto=line.price_subtotal
-                elif 'College Magazine' in line.product_id.name:
-                    self.collegemagazine=line.price_subtotal
-                elif 'Continuation' in line.product_id.name:
-                    self.continuation=line.price_subtotal
-                elif 'Discipline' in line.product_id.name:
-                    self.dc=line.price_subtotal
-                elif 'Examination' in line.product_id.name:
-                    self.ec=line.price_subtotal
-                elif 'Farewell' in line.product_id.name:
-                    self.farewell=line.price_subtotal
-                elif 'ID Card Fine' in line.product_id.name:
-                    self.idcardfine=line.price_subtotal
-                elif 'Late Coming' in line.product_id.name:
-                    self.latecoming=line.price_subtotal
-                elif 'Late Fee' in line.product_id.name:
-                    self.latefee=line.price_subtotal
-                elif 'ID Card' in line.product_id.name:
-                    self.idcard=line.price_subtotal
-                elif 'Gate Pass' in line.product_id.name:
-                    self.gatepass=line.price_subtotal
-                elif 'Miscellaneous & Fine' in line.product_id.name:
-                    self.mnf=line.price_subtotal
-                elif 'Mobile Fine' in line.product_id.name:
-                    self.mobfine=line.price_subtotal
-                elif 'Newsletter' in line.product_id.name:
-                    self.news=line.price_subtotal
-                elif 'Paragon 2nd Child and Onwards' in line.product_id.name:
-                    self.paragon=line.price_subtotal
-                elif 'Photocopy (Books)' in line.product_id.name:
-                    self.books=line.price_subtotal
-                elif 'Photocopying Charges' in line.product_id.name:
-                    self.pcopy=line.price_subtotal
-                elif 'Photograph' in line.product_id.name:
-                    self.photo=line.price_subtotal
-                elif 'Scarf' in line.product_id.name:
-                    self.scarf=line.price_subtotal
-                elif 'Sports Day' in line.product_id.name:
-                    self.sportd=line.price_subtotal
-                elif 'Stationary Charges' in line.product_id.name:
-                    self.stationary=line.price_subtotal
-                elif 'Uniform Fine' in line.product_id.name:
-                    self.uniform=line.price_subtotal
-                elif 'Wellcome Party' in line.product_id.name:
-                    self.welcome=line.price_subtotal
-                elif 'Work Books' in line.product_id.name:
-                    self.workbook=line.price_subtotal
-                elif 'Library Fine' in line.product_id.name:
-                    self.libfine=line.price_subtotal
-                elif line.product_id.x_studio_code=='ART':
-                    self.art=line.price_subtotal
-                elif line.product_id.x_studio_code=='COM':
-                    self.computing=line.price_subtotal
-                elif line.product_id.x_studio_code=='CHM':
-                    self.chemistry=line.price_subtotal
-                elif line.product_id.x_studio_code=='PHY':
-                    self.physics=line.price_subtotal
-                elif line.product_id.x_studio_code=='BIO':
-                    self.biology=line.price_subtotal
-            
-        wholename=""
-        if self.student_ids.homeroom:
-            wholename=self.student_ids.homeroom
-            splitted_name=wholename.split('-')
-            if len(splitted_name)>2:
-                #self.class_name=splitted_name[0]+"-"+splitted_name[1]
-                self.class_name=self.grade_level_ids.name
-                self.section_name=splitted_name[2]
-            elif len(splitted_name)>1:
-                #self.class_name=splitted_name[0]
-                self.class_name=self.grade_level_ids.name
-                self.section_name=splitted_name[1]
-            elif len(splitted_name)>0:
+                    elif 'Class Photo' in line.product_id.name:
+                        rec_inv.classphoto=line.price_subtotal
+                    elif 'College Magazine' in line.product_id.name:
+                        rec_inv.collegemagazine=line.price_subtotal
+                    elif 'Continuation' in line.product_id.name:
+                        rec_inv.continuation=line.price_subtotal
+                    elif 'Discipline' in line.product_id.name:
+                        rec_inv.dc=line.price_subtotal
+                    elif 'Examination' in line.product_id.name:
+                        rec_inv.ec=line.price_subtotal
+                    elif 'Farewell' in line.product_id.name:
+                        rec_inv.farewell=line.price_subtotal
+                    elif 'ID Card Fine' in line.product_id.name:
+                        rec_inv.idcardfine=line.price_subtotal
+                    elif 'Late Coming' in line.product_id.name:
+                        rec_inv.latecoming=line.price_subtotal
+                    elif 'Late Fee' in line.product_id.name:
+                        rec_inv.latefee=line.price_subtotal
+                    elif 'ID Card' in line.product_id.name:
+                        rec_inv.idcard=line.price_subtotal
+                    elif 'Gate Pass' in line.product_id.name:
+                        rec_inv.gatepass=line.price_subtotal
+                    elif 'Miscellaneous & Fine' in line.product_id.name:
+                        rec_inv.mnf=line.price_subtotal
+                    elif 'Mobile Fine' in line.product_id.name:
+                        rec_inv.mobfine=line.price_subtotal
+                    elif 'Newsletter' in line.product_id.name:
+                        rec_inv.news=line.price_subtotal
+                    elif 'Paragon 2nd Child and Onwards' in line.product_id.name:
+                        rec_inv.paragon=line.price_subtotal
+                    elif 'Photocopy (Books)' in line.product_id.name:
+                        rec_inv.books=line.price_subtotal
+                    elif 'Photocopying Charges' in line.product_id.name:
+                        rec_inv.pcopy=line.price_subtotal
+                    elif 'Photograph' in line.product_id.name:
+                        rec_inv.photo=line.price_subtotal
+                    elif 'Scarf' in line.product_id.name:
+                        rec_inv.scarf=line.price_subtotal
+                    elif 'Sports Day' in line.product_id.name:
+                        rec_inv.sportd=line.price_subtotal
+                    elif 'Stationary Charges' in line.product_id.name:
+                        rec_inv.stationary=line.price_subtotal
+                    elif 'Uniform Fine' in line.product_id.name:
+                        rec_inv.uniform=line.price_subtotal
+                    elif 'Wellcome Party' in line.product_id.name:
+                        rec_inv.welcome=line.price_subtotal
+                    elif 'Work Books' in line.product_id.name:
+                        rec_inv.workbook=line.price_subtotal
+                    elif 'Library Fine' in line.product_id.name:
+                        rec_inv.libfine=line.price_subtotal
+                    elif line.product_id.x_studio_code=='ART':
+                        rec_inv.art=line.price_subtotal
+                    elif line.product_id.x_studio_code=='COM':
+                        rec_inv.computing=line.price_subtotal
+                    elif line.product_id.x_studio_code=='CHM':
+                        rec_inv.chemistry=line.price_subtotal
+                    elif line.product_id.x_studio_code=='PHY':
+                        rec_inv.physics=line.price_subtotal
+                    elif line.product_id.x_studio_code=='BIO':
+                        rec_inv.biology=line.price_subtotal
                 
-                self.class_name=self.grade_level_ids.name
+            wholename=""
+            if rec_inv.student_ids.homeroom:
+                wholename=rec_inv.student_ids.homeroom
+                splitted_name=wholename.split('-')
+                if len(splitted_name)>2:
+                    #self.class_name=splitted_name[0]+"-"+splitted_name[1]
+                    rec_inv.class_name=rec_inv.grade_level_ids.name
+                    rec_inv.section_name=splitted_name[2]
+                elif len(splitted_name)>1:
                     #self.class_name=splitted_name[0]
-        else:
-            if self.grade_level_ids:
-                self.class_name=self.grade_level_ids.name
+                    rec_inv.class_name=rec_inv.grade_level_ids.name
+                    rec_inv.section_name=splitted_name[1]
+                elif len(splitted_name)>0:
+                    
+                    rec_inv.class_name=rec_inv.grade_level_ids.name
+                        #self.class_name=splitted_name[0]
             else:
-                if self.journal_id.id==119:
-                    for nxt_grade in self.student_ids.enrollment_state_ids:
-                        self.class_name=nxt_grade.next_grade_level_id.name
+                if rec_inv.grade_level_ids:
+                    rec_inv.class_name=rec_inv.grade_level_ids.name
+                else:
+                    if rec_inv.journal_id.id==119:
+                        for nxt_grade in rec_inv.student_ids.enrollment_state_ids:
+                            rec_inv.class_name=nxt_grade.next_grade_level_id.name
 
-        wholedate=str(self.invoice_date)
-        splitted_name=wholedate.split('-')
-        if self.journal_id.id==125 :
-            if len(splitted_name)>2:
-                month=splitted_name[1]
-                wholeyear=splitted_name[0]
-                year=wholeyear[2:4]
-                if month =='01':
-                    self.bill_date="Jan"+"-"+year
-                elif month =='02':
-                    self.bill_date="Feb"+"-"+year
-                elif month =='03':
-                    self.bill_date="Mar"+"-"+year
-                elif month =='04':
-                    self.bill_date="Apr"+"-"+year
-                elif month =='05':
-                    self.bill_date="May"+"-"+year
-                elif month =='06':
-                    self.bill_date="Jun"+"-"+year
-                elif month =='07':
-                    self.bill_date="Jul"+"-"+year
-                elif month =='08':
-                    self.bill_date="Aug"+"-"+year
-                elif month =='09':
-                    self.bill_date="Sep"+"-"+year
-                elif month =='10':
-                    self.bill_date="Oct"+"-"+year
-                elif month =='11':
-                    self.bill_date="Nov"+"-"+year
-                elif month =='12':
-                    self.bill_date="Dec"+"-"+year
-        
-        if self.journal_id.id==126:
-            self.bill_date=self.bi_monthly_cycle
+            wholedate=str(rec_inv.invoice_date)
+            splitted_name=wholedate.split('-')
+            if rec_inv.journal_id.id==125 :
+                if len(splitted_name)>2:
+                    month=splitted_name[1]
+                    wholeyear=splitted_name[0]
+                    year=wholeyear[2:4]
+                    if month =='01':
+                        rec_inv.bill_date="Jan"+"-"+year
+                    elif month =='02':
+                        rec_inv.bill_date="Feb"+"-"+year
+                    elif month =='03':
+                        rec_inv.bill_date="Mar"+"-"+year
+                    elif month =='04':
+                        rec_inv.bill_date="Apr"+"-"+year
+                    elif month =='05':
+                        rec_inv.bill_date="May"+"-"+year
+                    elif month =='06':
+                        rec_inv.bill_date="Jun"+"-"+year
+                    elif month =='07':
+                        rec_inv.bill_date="Jul"+"-"+year
+                    elif month =='08':
+                        rec_inv.bill_date="Aug"+"-"+year
+                    elif month =='09':
+                        rec_inv.bill_date="Sep"+"-"+year
+                    elif month =='10':
+                        rec_inv.bill_date="Oct"+"-"+year
+                    elif month =='11':
+                        rec_inv.bill_date="Nov"+"-"+year
+                    elif month =='12':
+                        rec_inv.bill_date="Dec"+"-"+year
+            
+            if rec_inv.journal_id.id==126:
+                rec_inv.bill_date=rec_inv.bi_monthly_cycle
 
     @api.onchange('x_student_id_cred',"student_ids")
     def _students_onchange(self):
