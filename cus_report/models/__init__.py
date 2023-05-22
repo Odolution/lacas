8f67915cd4cbce79e6ffe83cafa3530929c78f19
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-05-22 17:06:31 +0500

    update sorting

--- cus_report/models/__init__.py
+++ cus_report/models/__init__.py
@@ -1,1 +1,2 @@
 from . import main_view
+from . import sorted_printing
---
+++ cus_report/models/sorted_printing.py
@@ -0,0 +1,31 @@
+from odoo import models, fields,api
+from odoo.exceptions import UserError
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+from odoo import http
+from odoo.http import request
+from datetime import datetime 
+
+class Sorted_cl(models.Model):
+    _inherit = 'account.move'
+
+    # challan_date = fields.Date(string='Challan Date')
+    # x_studio_current_branchschool = fields.Many2one('your.branch.model', string='Branch')
+    # class_name = fields.Many2one('your.class.model', string='Class')
+    # section_name = fields.Many2one('your.section.model', string='Section')
+
+    
+    def get_sorted_records(records):
+        #records = self.search([])
+        sorted_records = sorted(records, key=lambda x: (x.challan_date, x.x_studio_current_branchschool.name))
+        return sorted_records
+
+    def generate_report():
+        records = self.search([])
+
+        # Sort the records
+        sorted_records = sort_records(records)
+
+        # Pass the sorted records to the report generation method
+        report = env.ref('cus_report.fee_challan_student_wise').render_qweb_pdf(sorted_records)
+         # Do further processing or return the report as needed
