740ffcb984ac8d52487f6a08efb846f20a9e9dc6
fahadraisfahad <fahadraisfahad@gmail.com>
2023-07-03 14:53:34 +0500

    create model

---
+++ add_tution_pan_field/models/add_tution_plan.py
@@ -0,0 +1,10 @@
+from odoo import models, api, fields, _
+from odoo.exceptions import UserError
+import json
+import datetime
+
+
+# class AddTutionplan(models.Model):
+#     _name='add.tution.plan'
+
+
