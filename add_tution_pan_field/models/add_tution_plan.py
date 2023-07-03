c0c17b7871876e4cfd03769c094b3d70fa859290
fahadraisfahad <fahadraisfahad@gmail.com>
2023-07-03 19:50:53 +0500

    a

--- add_tution_pan_field/models/add_tution_plan.py
+++ add_tution_pan_field/models/add_tution_plan.py
@@ -1,11 +1,11 @@
 from odoo import models, api, fields, _
 from odoo.exceptions import UserError
-import json
-import datetime
+# import json
+# import datetime
 
 
 class add_tution_plan_wiz(models.TransientModel):
-    _name='tuition.add_tution_plan_wiz'
+    _name='add.tution.plan.wiz'
 
     plan_ids = fields.Many2many('tuition.plan', string='tuition_plan')
     # tution_template_id = fields.Many2one('tution.template', string='Tution Template')
