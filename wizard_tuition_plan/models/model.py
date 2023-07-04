681e6263723f8942d5c973d361f76342b8a2fd6f
fahadraisfahad <fahadraisfahad@gmail.com>
2023-07-04 12:31:27 +0500

    add fuction

--- wizard_tuition_plan/models/model.py
+++ wizard_tuition_plan/models/model.py
@@ -7,4 +7,9 @@ class wizard_tuition_plan(models.TransientModel):
     plan_ids = fields.Many2many('tuition.plan', string='tuition_plan')
     tuition_template_id = fields.Many2one('tuition.template', string='Tuition Template')
 
-    
+    def default_get(self, fields_list):
+        # OVERRIDE
+        res = super().default_get(fields_list)
+        ids=self._context.get("active_ids")
+        res["plan_ids"]=[(6,0,ids)]
+        return res
