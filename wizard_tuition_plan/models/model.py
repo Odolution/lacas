7cc5fa543b92b99fe086b75a83d11ebfd9067704
fahadraisfahad <fahadraisfahad@gmail.com>
2023-07-04 13:18:46 +0500

    aa

--- wizard_tuition_plan/models/model.py
+++ wizard_tuition_plan/models/model.py
@@ -7,6 +7,22 @@ class wizard_tuition_plan(models.TransientModel):
     plan_ids = fields.Many2many('tuition.plan', string='tuition_plan')
     tuition_template_id = fields.Many2one('tuition.template', string='Tuition Template')
 
+    
+    # def apply(self):
+    #     for lines in plan_ids:
+    
+    
+    @api.onchange('template_id')
+    def onchange_template_id(self):
+        if self.template_id:
+            raise UserError("same")
+            self.plan_ids = self.template_id.plan_ids
+        else:
+            self.plan_ids = False
+
+    
+    
+    
     def default_get(self, fields_list):
         # OVERRIDE
         res = super().default_get(fields_list)
