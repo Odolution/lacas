fb1230bcae9255fd1cae01498e89b931a61432f3
fahadraisfahad <fahadraisfahad@gmail.com>
2023-07-04 19:34:38 +0500

    s

--- wizard_tuition_plan/models/model.py
+++ wizard_tuition_plan/models/model.py
@@ -69,6 +69,7 @@ class wizard_tuition_plan(models.TransientModel):
                         'installment_ids':line.installment_ids.name,
                         'account_id': line.account_id.id,
                         # Add other field values as needed
+
                         
                     })
                     added_product_ids.add(line.product_id.id)
