0373cc116cea808441ff0a0cfd1e63cea9562e80
Nadia Faqir Muhammad <46475381+NadiaFM@users.noreply.github.com>
2023-07-04 18:35:54 +0500

    fix

--- wizard_tuition_plan/models/model.py
+++ wizard_tuition_plan/models/model.py
@@ -4,44 +4,68 @@ import json
 import datetime
 
 
+# class wizard_tuition_plan(models.TransientModel):
+#     _name='tuition.wizard_tuition_plan'
+
+#     plan_ids = fields.Many2many('tuition.plan', string='tuition_plan')
+#     tuition_template_id = fields.Many2one('tuition.template', string='Tuition Template')
+
+    
+#     def apply(self):
+        
+#         tuition_lines=self.tuition_template_id.line_ids
+
+#         for t_plan in self.plan_ids:
+#             # onchange function
+#             if self.tuition_template_id:
+#                 t_plan.tuition_template_id=self.tuition_template_id
+
+#             for lines in t_plan.line_ids:
+#                 for line in tuition_lines:
+#                     if lines.product_id==line.product_id:
+#                         continue
+#                         if lines.unit_price!=line.unit_price:
+#                             lines.unit_price=line.unit_price
+#                     else:
+#                         # t_plan= self.env['tuition.plan'].browse(t_plan.id)
+#                         new_line = self.env['tuition.plan.line'].new({
+#                                 'product_id': line.product_id,
+#                                 'plan_id': t_plan.id,
+#                                 'currency_id':1,
+#                                 'name':line.name,
+#                                 # Add other field values as needed
+#                             })
+#                         t_plan.line_ids += new_line
+
 class wizard_tuition_plan(models.TransientModel):
-    _name='tuition.wizard_tuition_plan'
+    _name = 'tuition.wizard_tuition_plan'
 
     plan_ids = fields.Many2many('tuition.plan', string='tuition_plan')
     tuition_template_id = fields.Many2one('tuition.template', string='Tuition Template')
 
-    
     def apply(self):
-        
-        tuition_lines=self.tuition_template_id.line_ids
+        tuition_lines = self.tuition_template_id.line_ids
 
         for t_plan in self.plan_ids:
-            # onchange function
-            if self.tuition_template_id:
-                t_plan.tuition_template_id=self.tuition_template_id
+            added_product_ids = set(t_plan.line_ids.mapped('product_id.id'))
 
-            for lines in t_plan.line_ids:
-                for line in tuition_lines:
-                    if lines.product_id==line.product_id:
-                        break
-                        if lines.unit_price!=line.unit_price:
-                            lines.unit_price=line.unit_price
-                    else:
-                        # t_plan= self.env['tuition.plan'].browse(t_plan.id)
-                        new_line = self.env['tuition.plan.line'].new({
-                                'product_id': line.product_id,
-                                'plan_id': t_plan.id,
-                                'currency_id':1,
-                                'name':line.name,
-                                # Add other field values as needed
-                            })
-                        t_plan.line_ids += new_line
+            for line in tuition_lines:
+                if line.product_id.id in added_product_ids:
+                    continue
 
-
-    # @api.onchange('tuition_template_id')
-    # def _onchange_tuition_template_id(self):
-    #     self.line_ids=self.tuition_template_id.line_ids
-        
+                existing_line = t_plan.line_ids.filtered(lambda l: l.product_id == line.product_id)
+                if existing_line:
+                    if existing_line.unit_price != line.unit_price:
+                        existing_line.unit_price = line.unit_price
+                else:
+                    new_line = self.env['tuition.plan.line'].create({
+                        'product_id': line.product_id.id,
+                        'plan_id': t_plan.id,
+                        'currency_id': 1,
+                        'name': line.name,
+                        # Add other field values as needed
+                    })
+                    added_product_ids.add(line.product_id.id)
 
 
          
