2e3770593a5a1ae8a1cc005afe2c1a54707d3645
fahadraisfahad <fahadraisfahad@gmail.com>
2023-07-05 16:10:03 +0500

    a

--- wizard_tuition_plan/models/model.py
+++ wizard_tuition_plan/models/model.py
@@ -51,10 +51,15 @@ class wizard_tuition_plan(models.TransientModel):
         for line in self.tuition_template_id.line_ids:
             price[line.name] = line.unit_price
 
-        installment={}
-        for month in self.tuition_template_id.installment_ids:
-            installment[month.name]=month.installment_ids.id
+        # installment={}
+        # for month in self.tuition_template_id.installment_ids:
+        #     installment[month.name]=month.installment_ids.id
             # list.append(line.installment_ids.id)
+
+        installment = []
+
+        for month in self.installment_ids:
+            installment.append(month.name)
 
         for t_plan in self.plan_ids:
              # onchange function
@@ -87,8 +92,8 @@ class wizard_tuition_plan(models.TransientModel):
                         'quantity': line.quantity,
                         # 'discount': line.discount,
                         'unit_price': price.get(line.name),
-                        # 'installment_ids':t_plan.installment_ids.id,
-                        'installment_ids': installment.get(month.name),
+                        'installment_ids':t_plan.installment,
+                        # 'installment_ids': installment.get(month.name),
 
 
                         'account_id': line.product_id.property_account_income_id.id,
