5ed5257815c3a633dbec4528264aa4ef8473f2ff
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-07-27 19:07:17 +0500

    auto_grade

--- challan_field/models/main_view.py
+++ challan_field/models/main_view.py
@@ -120,6 +120,7 @@ class account_fields(models.Model):
                   if record.journal_id.id == 125:
                     new_no = school_code + record.env['ir.sequence'].next_by_code('monthly_bills')
                     rec['name']=new_no
+                    rec['x_studio_previous_class']=record.student_ids.name
                   if record.journal_id.id == 126:
                     new_no = school_code + record.env['ir.sequence'].next_by_code('bi_monthly')
                     rec['name']=new_no
