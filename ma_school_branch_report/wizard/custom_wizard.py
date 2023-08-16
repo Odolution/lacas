e21c571df5a66949fed59472f342d9df8342f63a
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2023-08-16 13:22:56 +0500

    d

--- ma_school_branch_report/wizard/custom_wizard.py
+++ ma_school_branch_report/wizard/custom_wizard.py
@@ -35,4 +35,74 @@ class RecoveryReportWizard(models.TransientModel):
 
 
      def action_print_excel_school_branch_report(self):
-        return
+        self.action_print_report()
+        if xlwt:
+
+            
+            filename = 'Recovery Report.xls'
+            # One sheet by partner
+            workbook = xlwt.Workbook()
+            # sheet = workbook.add_sheet(report_name[:31])
+            worksheet = workbook.add_sheet('Recovery Report')
+            
+
+            
+            style_title = xlwt.easyxf(
+            "font:bold on,; align: vertical center,horiz center; border: top thin, bottom thin, right thin, left thin")
+            red_style_title = xlwt.easyxf('pattern: pattern solid, fore_colour pale_blue;'
+            "font:bold on,; align: vertical center,horiz center; border: top thin, bottom thin, right thin, left thin")
+            yellow_style_title = xlwt.easyxf('pattern: pattern solid, fore_colour yellow;'
+            "font:bold on,; align: vertical center,horiz center; border: top thin, bottom thin, right thin, left thin")
+            lime_style_title = xlwt.easyxf('pattern: pattern solid, fore_colour lime;'
+            "font:bold on,; align: vertical center,horiz center; border: top thin, bottom thin, right thin, left thin")
+
+            grand_heading_style = xlwt.easyxf('pattern: pattern solid, fore_colour white;'
+                              'font: colour black, bold True;')
+
+            heading_style = xlwt.easyxf('align: vertical center,horiz center;')
+            
+            date_format = xlwt.XFStyle()
+            date_format.num_format_str = 'dd/mm/yyyy'
+
+            # worksheet.write_merge(0, 1, 0, 5,"LACAS SCHOOL NETWORK ",style=style_title)
+            # worksheet.write_merge(0, 1, 6, 11, "Billing Cycle wise recovery report", style=style_title)
+            
+            
+
+            worksheet.write_merge(0,1,0,0,"Billing Cycle.", style=red_style_title)
+            worksheet.write_merge(0,1,1,1,"Total Billing (Bills Issuance)",style=red_style_title)
+            worksheet.write_merge(0,1,2,2,"No of Std",style=red_style_title)
+            worksheet.write_merge(0,1,3,3,"Recovery",style=red_style_title)
+            worksheet.write_merge(0,1,4,4,"Percentage of Recovery on Amount",style=red_style_title)
+     
+      
+
+            row=2
+            for rec in self.account_recovery_report_line:
+                if rec:
+            
+                    worksheet.write_merge(row,row,0,0,rec.billing_cycle, style=style_title)
+                    worksheet.write_merge(row,row,1,1,rec.total_issuance,style=style_title)
+                    worksheet.write_merge(row,row,2,2,rec.no_of_std,style=style_title)
+                    worksheet.write_merge(row,row,3,3,rec.total_recovery,style=style_title)
+                    worksheet.write_merge(row,row,4,4,rec.recovery_percentage,style=style_title)
+   
+                    row+=1
+                  
+
+            fp = io.BytesIO()
+            workbook.save(fp)
+
+            export_id = self.env['sale.day.book.report.excel'].create({'excel_file': base64.encodestring(fp.getvalue()), 'file_name': filename})
+            res = {
+                    'view_mode': 'form',
+                    'res_id': export_id.id,
+                    'res_model': 'sale.day.book.report.excel',
+                    'type': 'ir.actions.act_window',
+                    'target':'new'
+                }
+            return res
+            
+        else:
+            raise Warning (""" You Don't have xlwt library.\n Please install it by executing this command :  sudo pip3 install xlwt""")
+        
