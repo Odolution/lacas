e000142209611eeea34ba501f10a0d2bd861dd2e
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2023-08-16 16:39:28 +0500

    d

--- ma_school_branch_report/wizard/custom_wizard.py
+++ ma_school_branch_report/wizard/custom_wizard.py
@@ -44,7 +44,7 @@ class RecoveryReportWizard(models.TransientModel):
         for rec in school_ids_raw:
             school_ids.append(rec)
         
-        raise UserError(school_ids)
+        # raise UserError(school_ids)
 
     def action_print_excel_school_branch_report(self):
         
@@ -139,96 +139,6 @@ class RecoveryReportWizard(models.TransientModel):
 
             # raise UserError(str(range_start)+" "+str(range_stop))
       
-            for i in range(range_start,range_stop+1):
-      
-                worksheet.write_merge(2,3,col,col+1,months[i][1],red_style_title)
-                # worksheet.write_merge(row,row,col,col+1,months[i][2])
-                col+=2
-
-            worksheet.write_merge(2,3,col,col+1,"Total", lime_style_title)   
-            
-                # print('col:',months[i][1], 'data:',months[i][2])
-
-            # row = 4
-            # sno = 1
-        
-            column = 27
-            row = 4
-            sn=1
-            for rec in self.account_report_line:
-                
-
-                
-                if rec:
-
-                    column = 27
-
-                    worksheet.write(row,0,sn)
-                    worksheet.write_merge(row,row,1,3,rec.record_id,heading_style)
-                    worksheet.write_merge(row,row,4,5,rec.branch_name,heading_style)
-                   
-
-                    data_month= {
-                        1:['01','JAN-22',rec.jan,'22'],
-                        2:['02','FEB-22',rec.feb,'22'],
-                        3:['03','MAR-22',rec.mar,'22'],
-                        4:['04','APR-22',rec.apr,'22'],
-                        5:['05','MAY-22',rec.may,'22'],
-                        6:['06','JUN-22',rec.jun,'22'],
-                        7:['07','JUL-22',rec.jul,'22'],
-                        8:['08','AUG-22',rec.aug,'22'],
-                        9:['09','SEP-22',rec.sep,'22'],
-                        10:['10','OCT-22',rec.oct,'22'],
-                        11:['11','NOV-22',rec.nov,'22'],
-                        12:['12','DEC-22',rec.dec,'22'],
-                        13:['01','JAN-23',rec.jan_2,'23'],
-                        14:['02','FEB-23',rec.feb_2,'23'],
-                        15:['03','MAR-23',rec.mar_2,'23'],
-                        16:['04','APR-23',rec.apr_2,'23'],
-                        17:['05','MAY-23',rec.may_2,'23'],
-                        18:['06','JUN-23',rec.jun_2,'23'],
-                        19:['07','JUL-23',rec.jul_2,'23'],
-                        20:['08','AUG-23',rec.aug_2,'23'],
-                        21:['09','SEP-23',rec.sep_2,'23'],
-                        22:['10','OCT-23',rec.oct_2,'23'],
-                        23:['11','NOV-23',rec.nov_2,'23'],
-                        24:['12','DEC-23',rec.dec_2,'23'],
-                    }
-                    range_start = 0
-                    range_stop = 0
-                  
-                    for key, value in data_month.items():
-                        if value[0] == v_from_month and value[3] == v_from_year:
-                            range_start = key
-                        if value[0] == v_to_month and value[3] == v_to_year:
-                            range_stop = key
-                    
-
-                    for i in range(range_start,range_stop+1):
-                        # raise UserError(column)
-                        worksheet.write_merge(row,row,column,column+1,data_month[i][2],heading_style)
-                        # worksheet.write_merge(row,row,column,column+1,rec.total_amount)
-                        # lst.append([row_1,row_1,column,column+1])
-                      
-                        column+=2
-                    if rec.total_amount != 0:
-                        worksheet.write_merge(row,row,column,column+1,rec.total_amount,heading_style)
-                      
-                    row+=1
-                    sn+=1
-
-            fp = io.BytesIO()
-            workbook.save(fp)
-
-            export_id = self.env['sale.day.book.report.excel'].create({'excel_file': base64.encodestring(fp.getvalue()), 'file_name': filename})
-            res = {
-                    'view_mode': 'form',
-                    'res_id': export_id.id,
-                    'res_model': 'sale.day.book.report.excel',
-                    'type': 'ir.actions.act_window',
-                    'target':'new'
-                }
-            return res
             
         else:
             raise Warning (""" You Don't have xlwt library.\n Please install it by executing this command :  sudo pip3 install xlwt""")
