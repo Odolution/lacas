f1e790e3bdd81ffd5abd1638ac1d885133efbfcf
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2023-08-17 18:44:21 +0500

    d

--- ma_school_branch_report/wizard/custom_wizard.py
+++ ma_school_branch_report/wizard/custom_wizard.py
@@ -286,6 +286,7 @@ class RecoveryReportWizard(models.TransientModel):
                             group_total+=rec.school_bill_len
                         else:
                             worksheet.write_merge(row+1,row+1,col,col+1,group_total, style=red_style_title)
+                            raise UserError(str(group_name_list)+"==="+str(group_total)+" =="+str(row))
                             row+=1
                             group_name_list.clear()
                             group_total=0
