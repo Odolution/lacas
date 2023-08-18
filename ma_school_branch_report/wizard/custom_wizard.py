b68b86f8dbe9fe8f656a1ba881dc525b33c6b3c0
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2023-08-18 17:23:44 +0500

    e

--- ma_school_branch_report/wizard/custom_wizard.py
+++ ma_school_branch_report/wizard/custom_wizard.py
@@ -348,7 +348,7 @@ class RecoveryReportWizard(models.TransientModel):
                             #     worksheet.write_merge(row,row,col,col+2,0,style=style_title)
                         # if check:
                         #     worksheet.write_merge(row,row,col,col+2,0,style=style_title)
-                        # col+=3
+                        col+=3
                     # raise UserError(message)
                     worksheet.write_merge(row,row,col,col+1,rec.school_bill_len,style=style_title)
                     worksheet.write_merge(row,row,col+2,col+4,rec.billing_list_paid,style=style_title)
