f3b284c7f25fd0d14fe6e14a0cb2dfe292b674e3
Muhammad Huzaifa Shaikh <57531626+Huzaifa552@users.noreply.github.com>
2023-09-04 03:42:17 -0700

    Add files via upload

---
+++ Filter_module/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Filter Field',
+    'version':'1.2',
+    'category':'Accounts',
+    'sequence':-100,
+    'depends':['account'],
+    'data':['views/filter_field.xml'],
+    'installable':True,
+    'application':True,
+    'auto_install':False,
+    'license':'LGPL-3',
+}
