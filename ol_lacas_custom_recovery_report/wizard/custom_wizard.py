3215c8bc67b9f70763dc71cd246d95709bbb60fa
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2023-09-07 12:11:44 +0500

    fix

--- ol_lacas_custom_recovery_report/wizard/custom_wizard.py
+++ ol_lacas_custom_recovery_report/wizard/custom_wizard.py
@@ -131,6 +131,7 @@ class RecoveryReportWizard(models.TransientModel):
             # raise UserError(len(for_by_month_inv_ids))
             scan_data_list = [] 
             a = ""
+            month_issuance2=0
             month_dict = {"January": 1,"Jan": 1,"February": 2,"Feb": 2,"March": 3,"Mar": 3,"April": 4,"Apr": 4,"May": 5,"June": 6,"Jun": 6,"July": 7,"Jul": 7,"August": 8,"Aug": 8,"September": 9,"Sep": 9,"October": 10,"Oct": 10,"November": 11,"Nov": 11,"December": 12,"Dec": 12}
             months_list = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
             
