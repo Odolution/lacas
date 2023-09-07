d69f1c0936c38ce3e48e5c7090cd7e2a68f80e74
Zain Siddiqui <57249519+zain2323@users.noreply.github.com>
2023-09-07 18:00:43 +0500

    Added ol_fetch_homeroom module
    1- For  syncing homeroom
    2- For fetching mother_cnic, father_cnic and grade level
    
    Two scheduled actions :
    1 - The first one runs hourly
    2- The second one runs on monthly basis

---
+++ ol_fetch_homeroom/__manifest__.py
@@ -0,0 +1,14 @@
+{
+  'name' : 'HomeRoom Sync',
+  'version' : '1.0.0',
+  'category' : 'Contacts',
+  'sequence': -100,
+  'summary' : 'Lacas Homeroom',
+  'description' : """Fetch Homeroom""",
+  'depends':  ['edoob'],
+  'data':  [],
+  'demo':[],
+  'application':  True,
+  'auto_install':  False,
+  'license': 'LGPL-3',
+}
