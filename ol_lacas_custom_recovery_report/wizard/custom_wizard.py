13775385044581fac4ec52899f3300130cc6153b
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2023-09-07 11:32:02 +0500

    fix

--- ol_lacas_custom_recovery_report/wizard/custom_wizard.py
+++ ol_lacas_custom_recovery_report/wizard/custom_wizard.py
@@ -128,7 +128,6 @@ class RecoveryReportWizard(models.TransientModel):
             else:
                 for_by_month_inv_ids=self.env['account.move'].search([('move_type','=','out_invoice'),('state','=','posted'),('journal_id','=',126),('x_studio_current_branchschool','=',self.one_branch.id),('invoice_date',">=",self.from_date),('invoice_date',"<=",self.to_date)])
             
-            raise UserError(len(selected_month))
             a = ""
             month_dict = {"January": 1,"Jan": 1,"February": 2,"Feb": 2,"March": 3,"Mar": 3,"April": 4,"Apr": 4,"May": 5,"June": 6,"Jun": 6,"July": 7,"Jul": 7,"August": 8,"Aug": 8,"September": 9,"Sep": 9,"October": 10,"Oct": 10,"November": 11,"Nov": 11,"December": 12,"Dec": 12}
             months_list = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
@@ -137,7 +136,7 @@ class RecoveryReportWizard(models.TransientModel):
             month, year = date_str.split('-')
             start = month_dict.get(month.capitalize())
 
-            date_str = selected_month[len(selected_month)]
+            date_str = selected_month[len(selected_month)-1]
             month_last, year_last = date_str.split('-')
             end = month_dict.get(month_last.capitalize())
             raise UserError(str(start)+" "+str(end))
