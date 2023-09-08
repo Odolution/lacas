223b169931be8f771cafe955d1bd8fac343a1718
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2023-09-08 16:45:20 +0500

    fix

--- ma_school_branch_report/wizard/custom_wizard.py
+++ ma_school_branch_report/wizard/custom_wizard.py
@@ -347,7 +347,7 @@ class RecoveryReportWizard(models.TransientModel):
                                 new_month_key = f"{rec.branch_name}-{months[i][3]}-{months[i][0]}"
                                 for month_key, count in billing_counts.items():
                                     if new_month_key==month_key:
-                                        key = f"{new_substring}-{months[i][3]}-{months[i][0]}"
+                                        key = f"{rec.branch_name}-{months[i][3]}-{months[i][0]}"
                                         row_month_total= months_total_dict.get(key, 0)+count
                                         months_total_dict.update({key: row_month_total})
                             
