858e41f730f227e6291ca96c5d3698b4144e9e92
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-09-08 15:39:45 +0500

    Update model.py

--- ol_custom_discount/models/model.py
+++ ol_custom_discount/models/model.py
@@ -90,6 +90,6 @@ class invoice_ext(models.Model):
                 # raise UserError(customer_recievable_amount)
 
 
-                # recievable_line.with_context(check_move_validity=False).write({"debit":customer_recievable_amount,"credit":0})
+                recievable_line.with_context(check_move_validity=False).write({"debit":customer_recievable_amount,"credit":0})
 
 
