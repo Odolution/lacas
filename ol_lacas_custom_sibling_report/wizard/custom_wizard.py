a8e00ebcc5d2442357a1bcecb4f7899a6a17041a
Muhammad Huzaifa Shaikh <57531626+Huzaifa552@users.noreply.github.com>
2023-09-11 03:05:06 -0700

    update student wizard

--- ol_lacas_custom_sibling_report/wizard/custom_wizard.py
+++ ol_lacas_custom_sibling_report/wizard/custom_wizard.py
@@ -102,7 +102,7 @@ class SiblingsReportWizard(models.TransientModel):
                 m_ph=''
 
                 if len(rec.student_ids)>1:
-                    for status in rec.student_ids.enrollment_status_ids:
+                    for status in rec.student_ids.x_last_enrollment_status_id:
                         if status.name=='Enrolled':
                             tot_child=(len(rec.student_ids))
                             parent_code=rec.facts_id
