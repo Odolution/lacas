9de1f294ec736795559c1da2773c44b7f04eab65
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2023-09-12 16:17:45 +0500

    fix

--- ma_school_branch_report/wizard/custom_wizard.py
+++ ma_school_branch_report/wizard/custom_wizard.py
@@ -240,40 +240,37 @@ class RecoveryReportWizard(models.TransientModel):
 
             total_count=0
             total_count_paid=0
-            for bill_rec in school_bill_ids:
-                invoice_date = bill_rec.invoice_date
-                month_in_invoice = invoice_date.strftime('%m')
-                year_in_invoice = invoice_date.strftime('%y')
-                
-                # Check if the invoice date is within the specified range
-                if v_from_year <= year_in_invoice <= v_to_year and v_from_month <= month_in_invoice <= v_to_month:
-                    # Create a key using the month and year
-                    month_key = f"{select_new}-{year_in_invoice}-{month_in_invoice}"
-                    
-                    if bill_rec.payment_state =="paid":
-                        if bill_rec.ol_payment_date:
-                            payment_date = bill_rec.ol_payment_date
-                            month_in_payment = payment_date.strftime('%m')
-                            year_in_payment = payment_date.strftime('%y')
+            for month_in_list in select_by_monthly_list:
+                for bill_rec in school_bill_ids:
+                    raise UserError(bill_rec.bill_date)
+                    if bill_rec.bill_date:
+                   # Create a key using the month and year
+                        month_key = f"{select_new}-{month_in_list}"
+                        
+                        if bill_rec.payment_state =="paid":
+                            if bill_rec.ol_payment_date:
+                                payment_date = bill_rec.ol_payment_date
+                                month_in_payment = payment_date.strftime('%m')
+                                year_in_payment = payment_date.strftime('%y')
 
-                            if pay_from_year <= year_in_payment <= pay_to_year and pay_from_month <= month_in_payment <= pay_to_month:
-                                total_count_paid += float(bill_rec.amount_total)
+                                if pay_from_year <= year_in_payment <= pay_to_year and pay_from_month <= month_in_payment <= pay_to_month:
+                                    total_count_paid += float(bill_rec.amount_total)
 
-                    if month_key in billing_counts:
-                        billing_counts[month_key] += float(bill_rec.amount_total)
-                        total_count += float(bill_rec.amount_total)
-                    else:
-                        billing_counts[month_key] = float(bill_rec.amount_total)
-                        total_count += float(bill_rec.amount_total)
+                        if month_key in billing_counts:
+                            billing_counts[month_key] += float(bill_rec.amount_total)
+                            total_count += float(bill_rec.amount_total)
+                        else:
+                            billing_counts[month_key] = float(bill_rec.amount_total)
+                            total_count += float(bill_rec.amount_total)
 
             billing_list_paid[select_new] = total_count_paid
             billing_list[select_new] = total_count
 
-            for year in range(int(v_from_year), int(v_to_year) + 1):
-                for month in range(int(v_from_month), int(v_to_month) + 1):
-                    month_key = f"{select_new}-{str(year)[-2:]}-{month:02}"
-                    if month_key not in billing_counts:
-                        billing_counts[month_key] = 0
+            # for year in range(int(v_from_year), int(v_to_year) + 1):
+            #     for month in range(int(v_from_month), int(v_to_month) + 1):
+            #         month_key = f"{select_new}-{str(year)[-2:]}-{month:02}"
+            #         if month_key not in billing_counts:
+            #             billing_counts[month_key] = 0
            
             # raise UserError(billing_counts)
 
