3e5586b5714ad482a890af86a3328273f62f96d3
Muhammad Huzaifa Shaikh <57531626+Huzaifa552@users.noreply.github.com>
2023-09-12 04:58:35 -0700

    update

--- security_amount_report/models/security_amount_report.py
+++ security_amount_report/models/security_amount_report.py
@@ -70,14 +70,13 @@ class SecurityAmountReport(models.Model):
             invoice_domain = [('move_type', '=', 'out_invoice'), ('journal_id', '=', 'Admission Challan'),('student_ids','in',student_ids)]
             all_invoice_objects = self.env['account.move'].search(invoice_domain)
 
-            raise UserError(all_invoice_objects)
+            # Add unique student ids to the set if product_id==Security
+            for line in all_invoice_objects.invoice_line_ids:
+                if line.product_id.name == "Security":
+                    unique_student_ids.add(all_invoice_objects.student_ids)
+            
+            raise UserError(unique_student_ids)
 
-            # Add unique student ids to the set if product_id==Securit
-            for student in all_students:
-                if student.id in all_invoice_objects.students_id:
-                    for line in individual_object.invoice_line_ids:
-                        if line.product_id.name == "Security" and individual_object.x_student_id_cred:
-                            unique_student_ids.add(individual_object.x_student_id_cred.id)
 
             # Step 3: Search for students in out_refund (Reversal) with product_id==Security
             refund_domain = [('move_type', '=', 'out_refund')]
