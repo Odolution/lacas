dd9eaab5cee22dedb7fa33967c663980b58368db
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-09-18 13:28:01 +0500

    fix payment reference

--- challan_field/models/main_view.py
+++ challan_field/models/main_view.py
@@ -137,7 +137,7 @@ class account_fields(models.Model):
                   if record.journal_id.id == 119:
                     new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
                     rec['name']=new_no
-                    rec['payment_reference']=str(record.name)
+                    record['payment_reference']=str(record.name)
                   if record.journal_id.id == 125:
                     new_no = school_code + record.env['ir.sequence'].next_by_code('monthly_bills')
                     rec['name']=new_no
@@ -158,7 +158,7 @@ class account_fields(models.Model):
                   if record.journal_id.id == 126:
                     new_no = school_code + record.env['ir.sequence'].next_by_code('bi_monthly')
                     rec['name']=new_no
-                    record['payment_reference']=record.name
+                    record['payment_reference']=str(record.name)
                     record['payment_reference']=new_no
                     record['x_studio_previous_class']=record.student_ids.grade_level_ids.name
                     record['x_studio_previous_branch']=record.student_ids.x_last_school_id.name
