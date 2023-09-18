71c2c298d74e625d1bfe0b2f9fb70de44c5d11e3
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-09-18 13:17:38 +0500

    fix

--- challan_field/models/main_view.py
+++ challan_field/models/main_view.py
@@ -137,11 +137,12 @@ class account_fields(models.Model):
                   if record.journal_id.id == 119:
                     new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
                     rec['name']=new_no
-                    rec['payment_reference']=rec.name
+                    rec['payment_reference']=str(rec.name)
                   if record.journal_id.id == 125:
                     new_no = school_code + record.env['ir.sequence'].next_by_code('monthly_bills')
                     rec['name']=new_no
-                    rec['payment_reference']=record.name
+                    raise UserError(str(record.name))
+                    rec['payment_reference']=str(record.name)
                     record['x_studio_previous_class']=record.student_ids.grade_level_ids.name
                     record['x_studio_previous_branch']=record.student_ids.x_last_school_id.name
                     record['x_studio_previous_batch']=record.student_ids.x_studio_batchsession
