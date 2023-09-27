58a4559ec4eb224d5c97994f63189bc07b1ec5e0
Zain Siddiqui <57249519+zain2323@users.noreply.github.com>
2023-09-27 18:39:25 +0500

    Update extend_contacts_fields.py

--- ol_fetch_homeroom/models/extend_contacts_fields.py
+++ ol_fetch_homeroom/models/extend_contacts_fields.py
@@ -98,6 +98,7 @@ class SchoolStudent(models.Model):
                         url = f"https://api.factsmgt.com/Classes/v2?filters=classId=={class_id}"
                         response = requests.request("GET", url, headers=headers).json()['results']
                         if len(response) > 0:
+                            raise UserError(str(response))
                             section = response[-1].get('section')
                             if section:
                                 homeroom = grade_level + '-' + section
