c8b4c462df519d316aa09c55f7a8b2ba9144e73e
Gitahzam <108136654+Gitahzam@users.noreply.github.com>
2023-10-17 15:45:06 +0500

    debugging

--- security_amount_report/models/security_amount_report.py
+++ security_amount_report/models/security_amount_report.py
@@ -57,7 +57,7 @@ class SecurityAmountReport(models.Model):
             
             row = 1
             serial_number = 1
-            # enrolled_students = self.env['school.student'].search([('enrollment_status_ids.id','=', 2)])
+            enrolled_students = self.env['school.student'].search([('enrollment_status_ids.id','=', 2)])
             # raise UserError(enrolled_students)
             for student in enrolled_students:
                 admission = self.env['account.move'].search([("move_type","=","out_invoice"),('journal_id.name','=','Admission Challan'), ("std_factsid","=",student.facts_id)], limit=1)
