507df308b939f18bb281c39a352ba0459ef64b68
Gitahzam <108136654+Gitahzam@users.noreply.github.com>
2023-10-17 17:13:26 +0500

    adding student record to grade level condition

--- security_amount_report/models/security_amount_report.py
+++ security_amount_report/models/security_amount_report.py
@@ -81,7 +81,9 @@ class SecurityAmountReport(models.Model):
                         else:
                             worksheet.write(row, 3, "N/A")
                         
-                        if admission.class_name:
+                        if student.x_studio_grade_level:
+                            worksheet.write(row, 4, student.x_studio_grade_level)
+                        elif admission.class_name:
                             worksheet.write(row, 4, admission.class_name)
                         else:
                             worksheet.write(row, 4, "N/A")
