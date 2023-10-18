1d9ab050beed4c6b1d1fd2729a5e9c3be8f8b09a
Gitahzam <108136654+Gitahzam@users.noreply.github.com>
2023-10-18 16:48:55 +0500

    Update security_amount_report.py

--- security_amount_report/models/security_amount_report.py
+++ security_amount_report/models/security_amount_report.py
@@ -134,7 +134,7 @@ class SecurityAmountReport(models.Model):
                          
                         if line.account_id.name == 'Security Fee':
                             worksheet.write(row, 9, line.price_total)
-                        if line.account_id.name != 'Security Fee':
+                        elif line.account_id.name != 'Security Fee':
                             for rev_line in reversal.invoice_line_ids:
                                 if rev_line.account_id.name == 'Security Fee':
                                     worksheet.write(row, 9, rev_line.price_total)
