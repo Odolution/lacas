3f4539b3a32ec13bd34e2764de7640d6535e33ec
maaz ali <58228528+Maaz-Ali2001@users.noreply.github.com>
2023-10-24 17:57:28 +0500

    api_billing

--- api_billing/models/main.py
+++ api_billing/models/main.py
@@ -593,7 +593,7 @@ class Billing(http.Controller):
 
                     },
 
-                    "Data": {"requestId":str(create_payment)},
+                    "Data": {"requestId":str(create_payment.read())},
 
                     "Navigation": {
 
