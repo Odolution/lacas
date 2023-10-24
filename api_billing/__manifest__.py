4cf60ab1555a34b7cf95d6ff49bb464e6b0426ee
maaz ali <58228528+Maaz-Ali2001@users.noreply.github.com>
2023-10-24 14:05:12 +0500

    Add api controller module for billing

---
+++ api_billing/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Billing Api',
+    'author': 'ol-Maaz Ali',
+    'version': '1.0.0',
+    'category': 'API controller',
+    'website':'',
+    'sequence': -100,
+    'summary': '',
+    'description': """""",
+    'depends': ['account','api_user'],
+    'data': [
+        'static/src/config/config.json',
+    ],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+    'license': 'LGPL-3',
+}
