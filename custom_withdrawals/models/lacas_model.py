77581fe0bb6069c4399d66604d872dc0ff4335fe
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-10-31 19:07:30 +0500

    fix

--- custom_withdrawals/models/lacas_model.py
+++ custom_withdrawals/models/lacas_model.py
@@ -44,7 +44,7 @@ class academics_tab(models.Model):
                 if rec.payment_state == 'paid':
                     rec.x_studio_withdrawn_status = 'Y'
             else:
-                reversal=rec.env['account.move'].search([(("move_type","=","out_refund"),("x_student_id_cred","=",rec.student_ids.id))])
+                reversal=rec.env['account.move'].search([(("move_type","=","out_refund"),("udid_cred_custom","=",rec.student_ids.facts_udid))])
                 if reversal:
                     for rev in reversal:
                         rec.x_studio_withdrawn_status = rev.x_studio_withdrawn_status
