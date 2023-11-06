58b778d180652415e97ab9880db4c252de5b51a3
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2023-11-06 16:12:32 +0500

    raise

--- ol_lacas_custom_report/wizard/custom_wizard.py
+++ ol_lacas_custom_report/wizard/custom_wizard.py
@@ -471,7 +471,7 @@ class ReceivablesReportWizard(models.TransientModel):
                         "dec_2": 0,
                         "total_amount":0
                     }
-            raise UserError(str(value.x_student_id_cred.facts_udid) )
+            raise UserError((value.student_id.facts_udid) )
             custom_dataa['full_roll_no'] = value.x_student_id_cred.facts_udid 
             if value.month_date == "January" and value.year_date=='22':
                 custom_dataa['jan'] = value.amount_residual
