6eaa133ee45aec1f628352541aea9580bde3d8a0
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2023-11-06 15:04:02 +0500

    raise

--- ol_lacas_custom_report/wizard/custom_wizard.py
+++ ol_lacas_custom_report/wizard/custom_wizard.py
@@ -216,7 +216,7 @@ class ReceivablesReportWizard(models.TransientModel):
                         "dec_2": 0,
                         "total_amount":0
                     }
-            raise UserError(value.x_studio_udid_monthly_bills)
+            # raise UserError(value.x_studio_udid_monthly_bills)
 
             if value.x_studio_udid_monthly_bills == "124959":
                 a=a+str(value.name)+"===="+str(move.amount_total)+"\n"
