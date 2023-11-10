f9c8c12e5243166ae9e7ef05316b270220fde3a6
maaz ali <58228528+Maaz-Ali2001@users.noreply.github.com>
2023-11-10 15:21:54 +0500

    ol_withdrawn_status_field

--- security_amount_report/models/security_amount_report.py
+++ security_amount_report/models/security_amount_report.py
@@ -119,10 +119,10 @@ class SecurityAmountReport(models.Model):
                             worksheet.write(row, 6, "N/A")
 
                         if admission.x_studio_withdrawn_status:
-                            worksheet.write(row, 7, admission.x_studio_withdrawn_status)
+                            worksheet.write(row, 7, admission.withdrawn_status)
                             
                         elif reversal.x_studio_withdrawn_status:
-                            worksheet.write(row, 7, reversal.x_studio_withdrawn_status)
+                            worksheet.write(row, 7, reversal.withdrawn_status)
                             
                         else:
                             worksheet.write(row, 7, "N/A")
