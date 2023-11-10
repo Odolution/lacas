77247a9053e5510efc42dab8a1472ccdd2b860c5
maaz ali <58228528+Maaz-Ali2001@users.noreply.github.com>
2023-11-10 13:51:56 +0500

    ol_withdrawn_status_field

---
+++ ol_withdrawn_status_field/models/model.py
@@ -0,0 +1,23 @@
+from odoo import api, fields, models
+from datetime import date
+from odoo.exceptions import UserError
+import math
+from odoo.exceptions import ValidationError
+from datetime import datetime
+
+class contacts(models.Model):
+    _inherit= 'account.move'
+
+    withdrawn_status = fields.Selection([('y', 'Y'), ('n', 'N')],string="Withdrawn Status", default="n",compute='_compute_withdrawn_status')
+
+
+    @api.depends('state','amount_residual')
+    def _compute_withdrawn_status(self):
+        for rec in self:
+            if rec['state']=='posted' and float(rec['amount_residual'])==0:
+                rec.withdrawn_status= 'n'
+
+            else:
+                pass
+
+
