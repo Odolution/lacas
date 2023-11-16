2f061e09eca61e23265d5d17a8bbc604f199a20a
maaz ali <58228528+Maaz-Ali2001@users.noreply.github.com>
2023-11-16 14:09:44 +0500

    ol_withdraw_status_field

--- ol_withdrawn_status_field/models/model.py
+++ ol_withdrawn_status_field/models/model.py
@@ -9,8 +9,11 @@ class Reversal(models.Model):
     _inherit= 'account.move' 
 
     withdrawn_status = fields.Selection([('Y', 'Y'), ('N', 'N')], compute='_compute_withdrawn_status',string="Withdrawn Status")
-    withdrawn_status_reversal = fields.Selection([('Y', 'Y'), ('N', 'N')], compute='_compute_withdrawn_status_reversal',string="Withdrawn Status",store=True)
-    withdrawn_status_bill = fields.Selection([('Y', 'Y'), ('N', 'N')],string="Withdrawn Status",store=True)
+    withdrawn_status_reversal_compute = fields.Selection([('Y', 'Y'), ('N', 'N')], compute='_compute_withdrawn_status_reversal',string="Withdrawn Status")
+    withdrawn_status_bill_compute = fields.Selection([('Y', 'Y'), ('N', 'N')],compute='_compute_withdrawn_status_bill',string="Withdrawn Status")
+    withdrawn_status_reversal = fields.Selection([('Y', 'Y'), ('N', 'N')],string="Withdrawn Status")
+    withdrawn_status_bill = fields.Selection([('Y', 'Y'), ('N', 'N')],string="Withdrawn Status")
+
 
 # , compute='_compute_withdrawn_status_bill'
 
@@ -27,9 +30,11 @@ class Reversal(models.Model):
     def _compute_withdrawn_status_reversal(self):
         for rec in self:
             if rec['state']=='posted' and float(rec['amount_residual'])==0:
+                rec.withdrawn_status_reversal_compute='Y'
                 rec.withdrawn_status_reversal= 'Y'
 
             else:
+                rec.withdrawn_status_reversal_compute='N'
                 rec.withdrawn_status_reversal= 'N'
 
     def _compute_withdrawn_status_bill(self):
@@ -37,11 +42,14 @@ class Reversal(models.Model):
             if rec['move_type']=='out_invoice' and rec['std_factsid']:
                 reversal= self.env['account.move'].search([('move_type','=','out_refund'),('facts_id_cred_custom','=',[rec['std_factsid'],rec['std_factsid2']]),('withdrawn_status_reversal','=','Y'),('state','=','posted')])
                 if reversal:
+                    rec.withdrawn_status_bill_compute='Y'
                     rec.withdrawn_status_bill= 'Y'
                 else:
+                    rec.withdrawn_status_bill_compute='N'
                     rec.withdrawn_status_bill= 'N'                    
 
             else:
+                rec.withdrawn_status_bill_compute='N'
                 rec.withdrawn_status_bill= 'N'
 
 
