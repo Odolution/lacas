6049e612b0cbba098336b9d1a6c952af341f2e5c
maaz ali <58228528+Maaz-Ali2001@users.noreply.github.com>
2023-11-16 12:17:34 +0500

    bill withdrawn status

--- ol_withdrawn_status_field/models/model.py
+++ ol_withdrawn_status_field/models/model.py
@@ -5,19 +5,32 @@ import math
 from odoo.exceptions import ValidationError
 from datetime import datetime
 
-class account(models.Model):
+class Reversal(models.Model):
     _inherit= 'account.move'
 
-    withdrawn_status = fields.Selection([('Y', 'Y'), ('N', 'N')],string="Withdrawn Status", compute='_compute_withdrawn_status')
+    withdrawn_status_reversal = fields.Selection([('Y', 'Y'), ('N', 'N')],string="Withdrawn Status", compute='_compute_withdrawn_status_reversal')
+    withdrawn_status_bill = fields.Selection([('Y', 'Y'), ('N', 'N')],string="Withdrawn Status", compute='_compute_withdrawn_status_bill')
+
 
 
     # @api.depends('state','amount_residual')
-    def _compute_withdrawn_status(self):
+    def _compute_withdrawn_status_reversal(self):
         for rec in self:
             if rec['state']=='posted' and float(rec['amount_residual'])==0:
-                rec.withdrawn_status= 'Y'
+                rec.withdrawn_status_reversal= 'Y'
 
             else:
-                rec.withdrawn_status= 'N'
+                rec.withdrawn_status_reversal= 'N'
+
+    def _compute_withdrawn_status_bill(self):
+        for rec in self:
+            if rec['move_type']=='out_invoice':
+                reversal= self.env['account.move'].search([('move_type','=','out_refund'),('facts_id_cred_custom','in',[rec['std_factsid'],rec['std_factsid2']]),('withdrawn_status_reversal','=','Y')])
+                rec.withdrawn_status_bill= 'Y'
+
+            else:
+                rec.withdrawn_status_bill= 'N'
+
+
 
 
