3c565d232633bf3ec6f68afce6080a3851eb2ded
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-11-18 18:26:29 +0500

    .

--- wizard_tuition_plan/models/model.py
+++ wizard_tuition_plan/models/model.py
@@ -82,8 +82,8 @@ class wizard_tuition_plan(models.TransientModel):
                 for line in t_plan.line_ids:
                     if combined_lines_not_to_remove:
                         for res in combined_lines_not_to_remove:
-                            raise UserError(res.id)
                             if line.id != res.id:
+                                raise UserError("true")
                                 line.unlink()
                     else:
                         line.unlink()
