115fc01ea4897081ab806cf48fbe1cdc231f5ffb
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-11-18 17:48:59 +0500

    .

--- wizard_tuition_plan/models/model.py
+++ wizard_tuition_plan/models/model.py
@@ -74,7 +74,7 @@ class wizard_tuition_plan(models.TransientModel):
             
 
             if t_plan.student_grade_level_ids.name == "XI":
-                lines_to_remove = t_plan.line_ids.filtered(lambda l: not (l.product_id.x_studio_code in ['ART', 'BIO', 'CHM', 'COM', 'PHY'] or not l.product_id.is_discount_type))
+                lines_to_remove = t_plan.line_ids.filtered(lambda l: not (l.product_id.x_studio_code in ['ART', 'BIO', 'CHM', 'COM', 'PHY'] and not l.product_id.is_discount_type))
                 raise UserError(str(lines_to_remove))
                 
                 lines_to_remove.unlink()
