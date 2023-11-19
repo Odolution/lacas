e88807a05d8c41e9ca087db3259a0bdb17b4937f
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-11-19 15:16:32 +0500

    update XI remove lines condition promotion

--- wizard_tuition_plan/models/model.py
+++ wizard_tuition_plan/models/model.py
@@ -74,8 +74,9 @@ class wizard_tuition_plan(models.TransientModel):
             
 
             if t_plan.student_grade_level_ids.name == "XI":
-                lines_to_remove = t_plan.line_ids.filtered(lambda l: l.product_id.x_studio_code in ['ART', 'BIO', 'CHM', 'COM', 'PHY'] or l.product_id.is_discount_type == 0)
-                lines_to_remove.unlink()
+                #lines_to_remove = t_plan.line_ids.filtered(lambda l: l.product_id.x_studio_code in ['ART', 'BIO', 'CHM', 'COM', 'PHY'] or l.product_id.is_discount_type == 0)
+                lines_to_override = t_plan.line_ids.filtered(lambda l: not (l.product_id.x_studio_code or l.product_id.is_discount_type))
+                lines_to_override.unlink()
             else:
                 non_discount_lines_to_remove =t_plan.line_ids.filtered(lambda l: l.product_id.x_studio_code not in ['ART', 'BIO', 'CHM', 'COM', 'PHY']  and l.product_id.is_discount_type == 0)
                 non_discount_lines_to_remove.unlink()
