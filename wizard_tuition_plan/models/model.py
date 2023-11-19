ade0213d80c0073edb5bc23c9511dc776bbe0a89
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-11-19 15:23:34 +0500

    remove grade 11 condition

--- wizard_tuition_plan/models/model.py
+++ wizard_tuition_plan/models/model.py
@@ -72,15 +72,15 @@ class wizard_tuition_plan(models.TransientModel):
             #     specialization_charges_remove.unlink()
 
             
-
-            if t_plan.student_grade_level_ids.name == "XI":
+         #fizra update start
+            #if t_plan.student_grade_level_ids.name == "XI":
                 #lines_to_remove = t_plan.line_ids.filtered(lambda l: l.product_id.x_studio_code in ['ART', 'BIO', 'CHM', 'COM', 'PHY'] or l.product_id.is_discount_type == 0)
-                lines_to_override = t_plan.line_ids.filtered(lambda l: not (l.product_id.x_studio_code or l.product_id.is_discount_type))
-                lines_to_override.unlink()
-            else:
-                non_discount_lines_to_remove =t_plan.line_ids.filtered(lambda l: l.product_id.x_studio_code not in ['ART', 'BIO', 'CHM', 'COM', 'PHY']  and l.product_id.is_discount_type == 0)
-                non_discount_lines_to_remove.unlink()
-
+            lines_to_override = t_plan.line_ids.filtered(lambda l: not (l.product_id.x_studio_code or l.product_id.is_discount_type))
+            lines_to_override.unlink()
+            # else:
+            #     non_discount_lines_to_remove =t_plan.line_ids.filtered(lambda l: l.product_id.x_studio_code not in ['ART', 'BIO', 'CHM', 'COM', 'PHY']  and l.product_id.is_discount_type == 0)
+            #     non_discount_lines_to_remove.unlink()
+        #fizra update end
                 # raise UserError("do not unlink")
 
             # if t_plan.student_grade_level_ids.name == "XI":
