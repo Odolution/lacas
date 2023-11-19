4afda2845568b30897aca5f4c702166cf354dcbb
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2023-11-19 14:45:25 +0500

    .

--- wizard_tuition_plan/models/model.py
+++ wizard_tuition_plan/models/model.py
@@ -74,7 +74,7 @@ class wizard_tuition_plan(models.TransientModel):
             
 
             if t_plan.student_grade_level_ids.name == "XI":
-                lines_to_override = t_plan.line_ids.filtered(lambda l: not (l.product_id.x_studio_code and l.product_id.is_discount_type))
+                lines_to_override = t_plan.line_ids.filtered(lambda l: not (l.product_id.x_studio_code | l.product_id.is_discount_type))
                 raise UserError(lines_to_override)
                 # lines_not_to_remove_discount = t_plan.line_ids.filtered(lambda l: l.product_id.is_discount_type)
                 # lines_not_to_remove_studio_code = t_plan.line_ids.filtered(lambda l: l.product_id.x_studio_code in ['ART', 'BIO', 'CHM', 'COM', 'PHY'])
