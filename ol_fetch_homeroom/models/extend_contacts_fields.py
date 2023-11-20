4d22de30258ae5fb9b2d18ebbaae8d360490957d
Muhammad Huzaifa Shaikh <57531626+Huzaifa552@users.noreply.github.com>
2023-11-20 12:02:20 +0500

    ...

--- ol_fetch_homeroom/models/extend_contacts_fields.py
+++ ol_fetch_homeroom/models/extend_contacts_fields.py
@@ -94,12 +94,12 @@ class SchoolStudent(models.Model):
             
             if len(std.grade_level_ids) >= 1:
                 grade_level = std.grade_level_ids[0].name
-                raise UserError(grade_level)
                 if grade_level:
                     url = f"https://api.factsmgt.com/academics/Enrollments?filters=studentId=={std.facts_id}"
                     response = requests.request("GET", url, headers=headers).json()['results']
                     if len(response) > 0:
                         record = response[-1]
+                        raise UserError(record)
                         class_id = record.get('classId')
                         url = f"https://api.factsmgt.com/Classes/v2?filters=classId=={class_id}"
                         response = requests.request("GET", url, headers=headers).json()['results']
