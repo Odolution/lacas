713b64924c4a29bee786d6e7318cfefede66da28
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2023-12-01 12:16:21 +0500

    .

--- ma_billing_cycle_report_v2/wizard/custom_wizard.py
+++ ma_billing_cycle_report_v2/wizard/custom_wizard.py
@@ -52,3 +52,62 @@ class RecoveryReportWizard(models.TransientModel):
     # account_report_line=fields.Many2many('billing.student.report.line', string='Account report Line')
     # by_account_report_line=fields.Many2many('student.bi.monthly.report.line', string='Account by Monthly report Line')
     
+    def action_print_excel_billing_report(self):
+        if xlwt:
+            global billing_counts ,by_monthly_billing_counts,select_by_monthly_list
+            
+            filename = 'Students Branch Report.xls'
+            # One sheet by partner
+            workbook = xlwt.Workbook()
+            # sheet = workbook.add_sheet(report_name[:31])
+            
+            style_title = xlwt.easyxf(
+            " align: vertical center,horiz center; border: top thin, bottom thin, right thin, left thin")
+            red_style_title = xlwt.easyxf('pattern: pattern solid, fore_colour tan;'
+            "font:bold on,; align: vertical center,horiz center; border: top thin, bottom thin, right thin, left thin")
+            yellow_style_title = xlwt.easyxf('pattern: pattern solid, fore_colour light_green;'
+            "font:bold on,; align: vertical center,horiz center; border: top thin, bottom thin, right thin, left thin")
+            lime_style_title = xlwt.easyxf('pattern: pattern solid, fore_colour lime;'
+            "font:bold on,; align: vertical center,horiz center; border: top thin, bottom thin, right thin, left thin")
+
+            grand_heading_style = xlwt.easyxf('pattern: pattern solid, fore_colour white;'
+                              'font: colour black, bold True;')
+
+            heading_style = xlwt.easyxf('align: vertical center,horiz center;')
+            
+            date_format = xlwt.XFStyle()
+            date_format.num_format_str = 'dd/mm/yyyy'
+
+            # worksheet.write_merge(0, 1, 0, 5,"LACAS SCHOOL NETWORK ",style=style_title)
+            # worksheet.write_merge(0, 1, 6, 11, "RECEIVABLE OF WITHDRAWAL STUDENTS", style=style_title)
+            
+            worksheet = workbook.add_sheet('Students Branch Std')
+
+            v_from_month=datetime.strptime(str(self.from_date), "%Y-%m-%d").strftime('%m')
+            v_from_year=datetime.strptime(str(self.from_date), "%Y-%m-%d").strftime('%y')
+
+            v_to_month=datetime.strptime(str(self.to_date), "%Y-%m-%d").strftime('%m')
+            v_to_year=datetime.strptime(str(self.to_date), "%Y-%m-%d").strftime('%y')   
+
+
+
+
+
+
+            fp = io.BytesIO()
+            workbook.save(fp)
+
+            export_id = self.env['sale.day.book.report.excel'].create({'excel_file': base64.encodestring(fp.getvalue()), 'file_name': filename})
+            res = {
+                    'view_mode': 'form',
+                    'res_id': export_id.id,
+                    'res_model': 'sale.day.book.report.excel',
+                    'type': 'ir.actions.act_window',
+                    'target':'new'
+                }
+            return res
+
+            
+        else:
+            raise Warning (""" You Don't have xlwt library.\n Please install it by executing this command :  sudo pip3 install xlwt""")
+        
