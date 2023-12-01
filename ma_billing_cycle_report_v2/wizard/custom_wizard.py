ee5260660b3d540176c85946f9edfa08bfc0ee78
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2023-12-01 11:42:46 +0500

    push Billing cycle Report v2

---
+++ ma_billing_cycle_report_v2/wizard/custom_wizard.py
@@ -0,0 +1,55 @@
+
+from odoo import models, api, fields, _
+# from odoo.exceptions import UserError
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import json
+
+import xlsxwriter
+_
+from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError
+import calendar
+
+import base64
+
+import io
+try:
+    import xlwt
+except ImportError:
+    xlwt = None
+
+
+
+class AccountMoveReport(models.TransientModel):
+    _name = 'billing.student.report.line'
+    
+    record_id=fields.Char('ID')
+    branch_name=fields.Char('name')
+    school_bill_len =fields.Float('Total')
+    billing_list_paid =fields.Float('Paid')
+
+# class ByMonthlyAccountMoveReport(models.TransientModel): 
+#     _name = 'billing.student.bi.monthly.report.line'
+
+# access_student_bi_monthly_report_line,access.student.bi.monthly.report.line,model_student_bi_monthly_report_line,,1,1,1,0
+    
+#     record_id=fields.Char('ID')
+#     branch_name=fields.Char('name')
+#     school_bill_len =fields.Float('Total')
+#     billing_list_paid =fields.Float('Paid')
+    
+
+class RecoveryReportWizard(models.TransientModel):
+    _name="billing.cycle.school.report.wizard"
+    _description='Print Percentage wise billing cycle summary report Wizard'
+
+    
+    from_date = fields.Date(string='From')
+    to_date = fields.Date(string='To')
+    from_date_pay = fields.Date(string='From')
+    to_date_pay = fields.Date(string='To')
+    
+    account_report_line=fields.Many2many('student.report.line', string='Account report Line')
+    by_account_report_line=fields.Many2many('student.bi.monthly.report.line', string='Account by Monthly report Line')
+    
