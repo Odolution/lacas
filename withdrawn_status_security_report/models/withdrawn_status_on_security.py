6f5f3cb09496679226e01f12fc90c62367e672e6
Ayesha Khalid <81487964+ayeshakhalid17@users.noreply.github.com>
2023-12-04 19:00:11 +0500

    .

--- withdrawn_status_security_report/models/withdrawn_status_on_security.py
+++ withdrawn_status_security_report/models/withdrawn_status_on_security.py
@@ -11,21 +11,19 @@ class WithdrawnStatusOnSecurity(models.Model):
 
     def _compute_withdrawn_status_for_security(self):
         withdrawn_computed_bool=False
-        adm_account_move = self.env['account.move'].search([('student_ids', '=', rec.id),('move_type', '=', 'out_invoice'),('journal_id.name', '=', 'Admission Challan')])
-        rev_account_move = self.env['account.move'].search([('x_student_id_cred', '=', rec.id),('move_type', '=', 'out_refund'),('journal_id.name', '=', 'Security Deposit')])
-            
-        for rec in self:
-            if adm_account_move:
-                rec.withdrawn_status_computed=adm_account_move.withdrawn_status_bill
-                rec.withdrawn_status_security=rec.withdrawn_status_computed
-                withdrawn_computed_bool=True
-            elif rev_account_move:
-                rec.withdrawn_status_computed=rev_account_move.withdrawn_status_reversal
-                rec.withdrawn_status_security=rec.withdrawn_status_computed
-                withdrawn_computed_bool=True
+        adm_account_move = env['account.move'].search([('student_ids', '=', rec.id),('move_type', '=', 'out_invoice'),('journal_id.name', '=', 'Admission Challan')])
+        rev_account_move = env['account.move'].search([('x_student_id_cred', '=', rec.id),('move_type', '=', 'out_refund'),('journal_id.name', '=', 'Security Deposit')])
+        if adm_account_move:
+            for student_record in adm_account_move:
+            if student_record['withdrawn_status_bill']:
+                self['withdrawn_status_security']=student_record['withdrawn_status_bill']
             else:
-                rec.withdrawn_status_computed='N/A'
-                rec.withdrawn_status_security=rec.withdrawn_status_computed
-                withdrawn_computed_bool=True
+                self['withdrawn_status_security']='N/A'
+        elif rev_account_move:
+            for student_record in rev_account_move:
+            if student_record['withdrawn_status_reversal']:
+                self['withdrawn_status_security']=student_record['withdrawn_status_reversal']
+            else:
+                self['withdrawn_status_security']='N/A'
                 
 
