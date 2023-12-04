db5c81b91b89a990be5cde5381c4bec3d2d46422
Ayesha Khalid <81487964+ayeshakhalid17@users.noreply.github.com>
2023-12-04 15:17:09 +0500

    .

--- withdrawn_status_security_report/models/withdrawn_status_on_security.py
+++ withdrawn_status_security_report/models/withdrawn_status_on_security.py
@@ -8,12 +8,15 @@ class WithdrawnStatusOnSecurity(models.Model):
     withdrawn_status_computed=fields.Char(compute='_compute_withdrawn_status_for_security')
     withdrawn_status_security=fields.Char()
 
+    @api.depends('account.move.')
     def _compute_withdrawn_status_for_security(self):
-        account_move = self.env['account.move'].search([('std_factsid', '=', self.facts_id)], limit=1)
+        account_move = self.env['account.move'].search([('std_factsid', '=', self.facts_id),('journal_id', '=', 'Admission Challan'),
+    ('move_type', '=', 'out_invoice')], limit=1)
 
         if account_move:
             self.withdrawn_status_computed = account_move.withdrawn_status_bill
             self.withdrawn_status_security = account_move.withdrawn_status_bill
         else:
             self.withdrawn_status_security = 'N/A'
+            
 
