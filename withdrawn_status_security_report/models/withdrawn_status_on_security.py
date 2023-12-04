d596bbb4cd71e36bca52170c5e8ceaa433c72255
Ayesha Khalid <81487964+ayeshakhalid17@users.noreply.github.com>
2023-12-04 19:29:25 +0500

    /

--- withdrawn_status_security_report/models/withdrawn_status_on_security.py
+++ withdrawn_status_security_report/models/withdrawn_status_on_security.py
@@ -12,26 +12,27 @@ class WithdrawnStatusOnSecurity(models.Model):
 
     def _compute_withdrawn_status_for_security(self):
         for rec in self:
-            raise UserError('here')
-        withdrawn_computed_bool=False
-        adm_account_move = env['account.move'].search([('student_ids', '=', rec.id),('move_type', '=', 'out_invoice'),('journal_id.name', '=', 'Admission Challan')])
-        rev_account_move = env['account.move'].search([('x_student_id_cred', '=', rec.id),('move_type', '=', 'out_refund'),('journal_id.name', '=', 'Security Deposit')])
-        if adm_account_move:
-            for student_record in adm_account_move:
-                if student_record['withdrawn_status_bill']:
-                    self.withdrawn_status_computed=student_record['withdrawn_status_bill']
-                    self.withdrawn_status_security=student_record['withdrawn_status_bill']
-                    self.withdrawn_computed_bool=True
-                else:
-                    self.withdrawn_status_security='N/A'
-                    self.withdrawn_computed_bool=True
-        elif rev_account_move:
-            for student_record in rev_account_move:
-                if student_record['withdrawn_status_reversal']:
-                    self.withdrawn_status_computed=student_record['withdrawn_status_reversal']
-                    self.withdrawn_status_security=student_record['withdrawn_status_reversal']
-                    self.withdrawn_computed_bool=True
-                else:
-                    self.withdrawn_status_security='N/A'
-                    self.withdrawn_computed_bool=True
+            self.withdrawn_status_security="skdjfhsdkj"
+            
+        # withdrawn_computed_bool=False
+        # adm_account_move = env['account.move'].search([('student_ids', '=', rec.id),('move_type', '=', 'out_invoice'),('journal_id.name', '=', 'Admission Challan')])
+        # rev_account_move = env['account.move'].search([('x_student_id_cred', '=', rec.id),('move_type', '=', 'out_refund'),('journal_id.name', '=', 'Security Deposit')])
+        # if adm_account_move:
+        #     for student_record in adm_account_move:
+        #         if student_record['withdrawn_status_bill']:
+        #             self.withdrawn_status_computed=student_record['withdrawn_status_bill']
+        #             self.withdrawn_status_security=student_record['withdrawn_status_bill']
+        #             self.withdrawn_computed_bool=True
+        #         else:
+        #             self.withdrawn_status_security='N/A'
+        #             self.withdrawn_computed_bool=True
+        # elif rev_account_move:
+        #     for student_record in rev_account_move:
+        #         if student_record['withdrawn_status_reversal']:
+        #             self.withdrawn_status_computed=student_record['withdrawn_status_reversal']
+        #             self.withdrawn_status_security=student_record['withdrawn_status_reversal']
+        #             self.withdrawn_computed_bool=True
+        #         else:
+        #             self.withdrawn_status_security='N/A'
+        #             self.withdrawn_computed_bool=True
 
