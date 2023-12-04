bcea46713ad0f7d6e8edcc9c31ad46ce33496803
Ayesha Khalid <81487964+ayeshakhalid17@users.noreply.github.com>
2023-12-04 19:05:15 +0500

    .

--- withdrawn_status_security_report/models/withdrawn_status_on_security.py
+++ withdrawn_status_security_report/models/withdrawn_status_on_security.py
@@ -15,18 +15,18 @@ class WithdrawnStatusOnSecurity(models.Model):
         rev_account_move = env['account.move'].search([('x_student_id_cred', '=', rec.id),('move_type', '=', 'out_refund'),('journal_id.name', '=', 'Security Deposit')])
         if adm_account_move:
             for student_record in adm_account_move:
-            if student_record['withdrawn_status_bill']:
-                self.withdrawn_status_security=student_record['withdrawn_status_bill']
-                self.withdrawn_computed_bool=True
-            else:
-                self.withdrawn_status_security='N/A'
-                self.withdrawn_computed_bool=True
+                if student_record['withdrawn_status_bill']:
+                    self.withdrawn_status_security=student_record['withdrawn_status_bill']
+                    self.withdrawn_computed_bool=True
+                else:
+                    self.withdrawn_status_security='N/A'
+                    self.withdrawn_computed_bool=True
         elif rev_account_move:
             for student_record in rev_account_move:
-            if student_record['withdrawn_status_reversal']:
-                self.withdrawn_status_security=student_record['withdrawn_status_reversal']
-                self.withdrawn_computed_bool=True
-            else:
-                self.withdrawn_status_security='N/A'
-                self.withdrawn_computed_bool=True
+                if student_record['withdrawn_status_reversal']:
+                    self.withdrawn_status_security=student_record['withdrawn_status_reversal']
+                    self.withdrawn_computed_bool=True
+                else:
+                    self.withdrawn_status_security='N/A'
+                    self.withdrawn_computed_bool=True
 
