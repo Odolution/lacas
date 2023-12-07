2bdffbd1c1d4c8215223f19960f8412313d4ab63
maaz ali <58228528+Maaz-Ali2001@users.noreply.github.com>
2023-12-07 14:57:58 +0500

    ol_withdrawn_status_field

--- ol_withdrawn_status_field/models/model.py
+++ ol_withdrawn_status_field/models/model.py
@@ -5,19 +5,41 @@ import math
 from odoo.exceptions import ValidationError
 from datetime import datetime
 
-class account(models.Model):
-    _inherit= 'account.move'
+class Account_move(models.Model):
+    _inherit= 'account.move' 
 
-    withdrawn_status = fields.Selection([('Y', 'Y'), ('N', 'N')],string="Withdrawn Status", compute='_compute_withdrawn_status')
+    withdrawn_status_reversal_compute = fields.Selection([('Y', 'Y'), ('N', 'N')], compute='_compute_withdrawn_status_reversal',string="Withdrawn Status")
+    withdrawn_status_bill_compute = fields.Selection([('Y', 'Y'), ('N', 'N')],compute='_compute_withdrawn_status_bill',string="Withdrawn Status")
+    withdrawn_status_reversal = fields.Selection([('Y', 'Y'), ('N', 'N')],string="Withdrawn Status")
+    withdrawn_status_bill = fields.Selection([('Y', 'Y'), ('N', 'N')],string="Withdrawn Status")
 
 
-    # @api.depends('state','amount_residual')
-    def _compute_withdrawn_status(self):
+
+
+    def _compute_withdrawn_status_reversal(self):
         for rec in self:
             if rec['state']=='posted' and float(rec['amount_residual'])==0:
-                rec.withdrawn_status= 'Y'
+                rec.withdrawn_status_reversal_compute='Y'
+                rec.withdrawn_status_reversal= 'Y'
 
             else:
-                rec.withdrawn_status= 'N'
+                rec.withdrawn_status_reversal_compute='N'
+                rec.withdrawn_status_reversal= 'N'
+
+
+    def _compute_withdrawn_status_bill(self):
+        for rec in self:
+            if rec['move_type']=='out_invoice' and rec['std_factsid']:
+                reversal= self.env['account.move'].search([('move_type','=','out_refund'), ('facts_id_cred_custom','=',rec['std_factsid']),('withdrawn_status_reversal','=','Y'),('state','=','posted')])
+                if reversal:
+                    rec.withdrawn_status_bill_compute='Y'
+                    rec.withdrawn_status_bill= 'Y'
+                else:
+                    rec.withdrawn_status_bill_compute='N'
+                    rec.withdrawn_status_bill= 'N'                    
+
+            else:
+                rec.withdrawn_status_bill_compute='N'
+                rec.withdrawn_status_bill= 'N'
 
 
