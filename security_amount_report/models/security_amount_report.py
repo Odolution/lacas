d630a019a2ff0a0016ed14efa65f01ddb91ae3c7
Yaminah Batool <59056513+Yaminah22@users.noreply.github.com>
2023-12-13 17:21:07 +0500

    /

--- security_amount_report/models/security_amount_report.py
+++ security_amount_report/models/security_amount_report.py
@@ -69,7 +69,7 @@ class SecurityAmountReport(models.Model):
                 ])
                 for move in accounts:
                     if move.x_student_id_cred.id not in unique_student_ids:
-                        unique_student_ids.append(move.x_student_id_cred.id)
+                        unique_student_ids.append(move.x_student_id_cred)
 
             # Convert unique_student_ids to tuple for use in the PostgreSQL query
                 unique_student_ids_tuple = tuple(unique_student_ids)
