694549346fef6f7021bee6a55b1fe05a5b566e92
Syed Muhammad Hamza Naveed <79695071+sm-hamza-naveed@users.noreply.github.com>
2023-12-13 18:58:15 +0500

    .

--- ol_lacas_aging_report/wizard/custom_wizard.py
+++ ol_lacas_aging_report/wizard/custom_wizard.py
@@ -369,9 +369,9 @@ class agingsReportWizard(models.TransientModel):
         lines=[]
 
 
-
+        raise UserError(str(branch_lst))
         for branch in branch_lst:
-            raise UserError(branch)
+            raise UserError(str(branch))
 
            
             branch_wise_inv=self.env['account.move'].search([('move_type','=','out_invoice'),('state','=','posted'),('program_ids','=',branch.id),('journal_id','=',125),("invoice_date",">=",self.date_from),("invoice_date","<=",self.date_to)])
