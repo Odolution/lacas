c4e20887303529838a8b5bd69ca697c57b114084
Graceful Skipper <49026436+kk05088@users.noreply.github.com>
2024-01-02 12:45:32 +0500

    corrected model

--- ol_bill_difference/models/difference.py
+++ ol_bill_difference/models/difference.py
@@ -5,9 +5,9 @@ class Difference_Bills(models.Model):
     # _name = 'difference'
     x_difference = fields.Float(string="Difference", compute='_compute_difference', digits=(10,2))
 
-@api.depends('net_amount', 'amount_total_signed')
-def _compute_difference(self):
-    for record in self:
-        record['x_difference'] = False
-        if record.net_amount != '' and record.amount_total_signed != '':
-            record['x_difference'] = float(record.net_amount) - float(record.amount_total_signed)
+    @api.depends('net_amount', 'amount_total_signed')
+    def _compute_difference(self):
+        for record in self:
+            record['x_difference'] = False
+            if record.net_amount != '' and record.amount_total_signed != '':
+                record['x_difference'] = float(record.net_amount) - float(record.amount_total_signed)
