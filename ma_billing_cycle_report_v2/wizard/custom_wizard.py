3045158c32f823de73eaaa5f43ca302331481c98
MohammadArsalan308 <121546169+MohammadArsalan308@users.noreply.github.com>
2024-01-03 10:54:41 +0500

    .

--- ma_billing_cycle_report_v2/wizard/custom_wizard.py
+++ ma_billing_cycle_report_v2/wizard/custom_wizard.py
@@ -300,7 +300,7 @@ class RecoveryReportWizard(models.TransientModel):
                             ('invoice_date', '<=', self.to_date),
                         ])
                     enrolled_unpaid_student_count = len(set(bills.mapped('student_ids_ol.id')))
-                    if b.startswith(match) and (b != 'lacas johar town boys' ) and (b != 'milestone model town senior campus' ) and (b != 'lACAS gulberg girls campus' ):
+                    if b.startswith(match) and (b != 'lacas johar town boys' ) and (b != 'milestone model town senior campus' ) and (b != 'lacas gulberg girls campus' ):
                         total_total_Issuance_billing_branch += rec.total_Issuance_billing
                         total_with_out_Withdrawn_billing_branch += rec.with_out_Withdrawn_billing
                         total_total_Recovery_paid_branch += rec.total_Recovery_paid
