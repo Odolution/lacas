91e1351102e62192cad9047ac0c383871c77b475
Syed Muhammad Arham Tahir <126342951+arham-tahir@users.noreply.github.com>
2024-01-04 14:45:30 +0500

    .

--- ol_lacas_custom_report/wizard/custom_wizard.py
+++ ol_lacas_custom_report/wizard/custom_wizard.py
@@ -358,8 +358,7 @@ class ReceivablesReportWizard(models.TransientModel):
            
 
             temp_lst.append(custom_data)
-
-        raise UserError(temp_lst[0]["apr_3"])
+            raise UserError(custom_data["apr_3"])
 
         for element in temp_lst:
             temp_dict={
