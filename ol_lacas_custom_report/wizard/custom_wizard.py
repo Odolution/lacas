ee3338ce4aa1c8cd288e468520645a794cf05691
Nadia Faqir Muhammad <46475381+NadiaFM@users.noreply.github.com>
2024-01-05 17:28:33 +0500

    changing format in excel

--- ol_lacas_custom_report/wizard/custom_wizard.py
+++ ol_lacas_custom_report/wizard/custom_wizard.py
@@ -888,7 +888,7 @@ class ReceivablesReportWizard(models.TransientModel):
         }
 
         )
-        raise UserError(str(final_lst))
+        # raise UserError(str(final_lst))
        
        
         
@@ -1038,7 +1038,7 @@ class ReceivablesReportWizard(models.TransientModel):
                     worksheet.write_merge(row,row,1,3,rec.record_id,heading_style)
                     worksheet.write_merge(row,row,4,5,rec.app_date,heading_style)
                     worksheet.write_merge(row,row,6,7,rec.roll_no,heading_style)
-                    worksheet.write_merge(row,row,8,9,rec.full_roll_number,heading_style)
+                    worksheet.write_merge(row,row,8,9,rec.full_roll_number,heading_style,text_style)
                     worksheet.write_merge(row,row,10,11,rec.name,heading_style)
                     worksheet.write_merge(row,row,12,13,rec.student_batch,heading_style)
                     worksheet.write_merge(row,row,14,16,rec.student_branch,heading_style)
