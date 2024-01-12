423f9f33846505b4d292518b4036b61e8932907f
Graceful Skipper <49026436+kk05088@users.noreply.github.com>
2024-01-12 12:20:19 +0500

    .

--- ol_custom_discount/models/model.py
+++ ol_custom_discount/models/model.py
@@ -41,6 +41,12 @@ class template_ext(models.Model):
 class invoice_ext(models.Model):
 
     _inherit = "account.move"
+
+    def get_security_charge(self):
+        for line in self.invoice_line_ids:
+            if line.account_id == 2462:
+                raise UserError(line.price_unit)
+        
     def applyDiscount(self):
         
         for rec in self:
