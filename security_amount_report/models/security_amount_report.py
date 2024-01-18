040678ac7248a54f9bbf4435ec88b0eca38b794a
Yaminah Batool <59056513+Yaminah22@users.noreply.github.com>
2024-01-18 16:12:41 +0500

    /

--- security_amount_report/models/security_amount_report.py
+++ security_amount_report/models/security_amount_report.py
@@ -152,25 +152,31 @@ class SecurityAmountReport(models.Model):
                     #     worksheet.write(row, 8, str(reversal.invoice_date))
                     # else:
                     #     worksheet.write(row, 8, "N/A")
-                    adm_flag = False
-                    bills = self.env['account.move'].search([('student_ids', '=', reversal.x_student_id_cred.id)])
-                    for bill in bills:
-                        if bill.journal_id.name == "Admission Challan":
-                            worksheet.write(row, 8, str(bill.invoice_date))
-                            adm_flag = True
-                            break
                     
-                    # not found admission date
-                    if adm_flag == False:
-                        for history in reversal.x_student_id_cred.enrollment_history_ids:
-                            if history.enrollment_status_id.name == "Admission":
-                                worksheet.write(row, 8, history.timestamp.strftime("%Y-%m-%d"))
-                                adm_flag = True
-                                break
-                    
-                    # still not found admission date
-                    if adm_flag == False:
+                    if reversal.x_studio_admission_date_1:
+                        worksheet.write(row, 8, str(reversal.x_studio_admission_date_1))
+                    else:
                         worksheet.write(row, 8, "N/A")
+                    # adm_flag = False
+
+                    # bills = self.env['account.move'].search([('student_ids', '=', reversal.x_student_id_cred.id)])
+                    # for bill in bills:
+                    #     if bill.journal_id.name == "Admission Challan":
+                    #         worksheet.write(row, 8, str(bill.invoice_date))
+                    #         adm_flag = True
+                    #         break
+                    
+                    # # not found admission date
+                    # if adm_flag == False:
+                    #     for history in reversal.x_student_id_cred.enrollment_history_ids:
+                    #         if history.enrollment_status_id.name == "Admission":
+                    #             worksheet.write(row, 8, history.timestamp.strftime("%Y-%m-%d"))
+                    #             adm_flag = True
+                    #             break
+                    
+                    # # still not found admission date
+                    # if adm_flag == False:
+                    #     worksheet.write(row, 8, "N/A")
 
 
                     flag = False
