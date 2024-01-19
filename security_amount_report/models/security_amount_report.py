96f81d78f2fe07a82f140e3f69489e6902bf6b6d
Syed Muhammad Arham Tahir <126342951+arham-tahir@users.noreply.github.com>
2024-01-19 17:18:51 +0500

    .

--- security_amount_report/models/security_amount_report.py
+++ security_amount_report/models/security_amount_report.py
@@ -143,7 +143,7 @@ class SecurityAmountReport(models.Model):
 
 
                     if reversal.withdrawn_status_reversal:
-                        raise UserError(reversal.withdrawn_status_reversal)
+                        # raise UserError(reversal.withdrawn_status_reversal)
                         worksheet.write(row, 7, reversal.withdrawn_status_reversal)
                         
                     else:
