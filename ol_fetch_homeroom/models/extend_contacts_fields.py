d2c8bed7822900b7e09081d7acf078d5a7c0e70f
Syed Muhammad Arham Tahir <126342951+arham-tahir@users.noreply.github.com>
2024-02-02 16:43:46 +0500

    .

--- ol_fetch_homeroom/models/extend_contacts_fields.py
+++ ol_fetch_homeroom/models/extend_contacts_fields.py
@@ -99,7 +99,6 @@ class SchoolStudent(models.Model):
                         url = f"https://api.factsmgt.com/Classes/v2?filters=classId=={class_id}"
                         response = requests.request("GET", url, headers=headers).json()['results']
                         if len(response) > 0:
-                            raise UserError(response)
                             section = response[-1].get('section')
                             if section:
                                 homeroom = grade_level + '-' + section
