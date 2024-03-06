6dbf64edb69418c6a0ea9e8edd0ab219b170d0f9
Muhammad Huzaifa Shaikh <57531626+Huzaifa552@users.noreply.github.com>
2024-03-06 18:19:54 +0500

    test recovery report

--- ol_lacas_custom_recovery_report/wizard/custom_wizard.py
+++ ol_lacas_custom_recovery_report/wizard/custom_wizard.py
@@ -108,10 +108,21 @@ class RecoveryReportWizard(models.TransientModel):
         selected_month = self.list_months()
         # raise UserError(selected_month)
         for month in selected_month:
+            ## Huzaifa ##
+            journal_list = []
+            journals = self.env['account.journal'].search([
+                '|',
+                ('name','=','Monthly Bills'),
+                ('name','=','Bi Monthly'),
+                ])
+            if journals:
+                for journal in journals:
+                    journal_list.append(journal.id)
+            raise UserError(journal_list)
             if self.all_branch==True:
-                inv_ids=self.env['account.move'].search([('move_type','=','out_invoice'),('journal_id','=',125),('state','=','posted'),('invoice_date',">=",self.from_date),('invoice_date',"<=",self.to_date)])
+                inv_ids=self.env['account.move'].search([('move_type','=','out_invoice'),('journal_id','in',journal_list),('state','=','posted'),('invoice_date',">=",self.from_date),('invoice_date',"<=",self.to_date)])
             else:
-                inv_ids=self.env['account.move'].search([('move_type','=','out_invoice'),('state','=','posted'),('journal_id','=',125),('x_studio_current_branchschool','=',self.one_branch.id),('invoice_date',">=",self.from_date),('invoice_date',"<=",self.to_date)])
+                inv_ids=self.env['account.move'].search([('move_type','=','out_invoice'),('state','=','posted'),('journal_id','in',journal_list),('x_studio_current_branchschool','=',self.one_branch.id),('invoice_date',">=",self.from_date),('invoice_date',"<=",self.to_date)])
             
             stud_lst=[]
             month_issuance=0
