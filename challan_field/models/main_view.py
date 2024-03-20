75d8c2185371a414796a991fdbadeb82d7cb4da6
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2024-03-20 15:14:17 +0500

    changes in sequence

--- challan_field/models/main_view.py
+++ challan_field/models/main_view.py
@@ -131,15 +131,16 @@ class account_fields(models.Model):
                 if seq == 0:
                   
                   school_code=""
-                  if record.school_ids:
-                    for school in record.school_ids:
-                      school_code = school.description
-                  if record.x_school_id_cred:
+                  if record.student_ids:
+                    for student in record.student_ids:
+                      school_code = student.x_last_school_id.description
+                  if record.x_student_id_cred:
                     for school in record.x_school_id_cred:
                       school_code = school.description
                  # new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
                   if record.journal_id.id == 119:
-                    new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
+                   # new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
+                    new_no = school_code + record.env['ir.sequence'].next_by_code('bills_overall')
                     rec['name']=new_no
 
                     ##Huzaifa
@@ -166,7 +167,7 @@ class account_fields(models.Model):
                         break
 
                   if record.journal_id.id == 125:
-                    new_no = school_code + record.env['ir.sequence'].next_by_code('monthly_bills')
+                    new_no = school_code + record.env['ir.sequence'].next_by_code('bills_overall')
                     rec['name']=new_no
                     record['payment_reference']=str(record.name)
                     ##Huzaifa
@@ -188,7 +189,7 @@ class account_fields(models.Model):
                       
 
                   if record.journal_id.id == 126:
-                    new_no = school_code + record.env['ir.sequence'].next_by_code('bi_monthly')
+                    new_no = school_code + record.env['ir.sequence'].next_by_code('bills_overall')
                     rec['name']=new_no
 
                     record['x_studio_previous_class']=record.student_ids.grade_level_ids.name
