06d697d1cc7bfd46f37b235b76050352b82eb314
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2024-03-21 11:33:45 +0500

    cleaning code

--- challan_field/models/main_view.py
+++ challan_field/models/main_view.py
@@ -133,9 +133,9 @@ class account_fields(models.Model):
                     for student in record.student_ids:
                       school_code += student.x_last_school_id.description
                       
-                  if record.x_student_id_cred:
-                    for school in record.x_school_id_cred:
-                      school_code += school.description
+                  # if record.x_student_id_cred:
+                  #   for school in record.x_school_id_cred:
+                  #     school_code += school.description
                  # new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
                   if record.journal_id.id == 119:
                    # new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
@@ -217,7 +217,10 @@ class account_fields(models.Model):
               if record.move_type == 'out_refund':
 #                 record['name'] = 'Draft'
                 if seq == 0:
-                  # raise UserError(record)
+                  if record.x_student_id_cred:
+                    for school in record.x_school_id_cred:
+                      school_code += school.description
+                  raise UserError(record)
                   if record.x_school_id_cred:
                     new_no = record.x_school_id_cred.description + record.env['ir.sequence'].next_by_code('security')
                     record.payment_reference = new_no
