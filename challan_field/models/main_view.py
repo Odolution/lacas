066aa0024269d569b507b9fcc5c9427ea97b8e5c
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2024-03-21 11:23:47 +0500

    changes in school code

--- challan_field/models/main_view.py
+++ challan_field/models/main_view.py
@@ -128,15 +128,14 @@ class account_fields(models.Model):
               if record.move_type == 'out_invoice':
 #                 record['name'] = 'Draft'
                 if seq == 0:
-                  
                   school_code=""
                   if record.student_ids:
                     for student in record.student_ids:
-                      school_code = student.x_last_school_id.description
-                      raise UserError(school_code)
+                      school_code += student.x_last_school_id.description
+                      
                   if record.x_student_id_cred:
                     for school in record.x_school_id_cred:
-                      school_code = school.description
+                      school_code += school.description
                  # new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
                   if record.journal_id.id == 119:
                    # new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
