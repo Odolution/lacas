0c590e4a3744bc57fffc662bf285c67d97ea3825
fizraazhar <105269198+fizraazhar@users.noreply.github.com>
2024-03-22 07:23:30 +0500

    .

--- challan_field/models/main_view.py
+++ challan_field/models/main_view.py
@@ -132,17 +132,18 @@ class account_fields(models.Model):
                   
                   
                   if record.school_ids:
-                    school_code=""
+                  
                     for school in record.school_ids:
                       school_code = school.description
                   if record.x_school_id_cred:
-                    school_code=""
+                    
                     for school in record.x_school_id_cred:
                       school_code = school.description
                  # new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
                   if record.journal_id.id == 119:
                     new_no = school_code + record.env['ir.sequence'].next_by_code('adm_challan')
                     rec['name']=new_no
+                    school_code=""
 
                     ##Huzaifa
                     record['x_studio_previous_class']=record.student_ids.x_studio_grade_level
@@ -171,6 +172,7 @@ class account_fields(models.Model):
                     new_no = school_code + record.env['ir.sequence'].next_by_code('monthly_bills')
                     rec['name']=new_no
                     record['payment_reference']=str(record.name)
+                    school_code=""
                     ##Huzaifa
                     
                     record['x_studio_previous_class']=record.student_ids.grade_level_ids.name
@@ -192,6 +194,7 @@ class account_fields(models.Model):
                   if record.journal_id.id == 126:
                     new_no = school_code + record.env['ir.sequence'].next_by_code('bi_monthly')
                     rec['name']=new_no
+                    school_code=""
 
                     record['x_studio_previous_class']=record.student_ids.grade_level_ids.name
                     record['x_studio_previous_branch']=record.student_ids.x_last_school_id.name
