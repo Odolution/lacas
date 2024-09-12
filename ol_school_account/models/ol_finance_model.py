cc7a123254786791ff21251a190efca1d080c563
Muhammad Huzaifa Shaikh <57531626+Huzaifa552@users.noreply.github.com>
2024-09-12 14:12:01 +0500

    check

--- ol_school_account/models/ol_finance_model.py
+++ ol_school_account/models/ol_finance_model.py
@@ -406,22 +406,24 @@ class TuitionPlanInstallment(models.Model):
             records.sudo().write({'is_move_sent': True})
 
     ##Huzaifa
-    # def _get_monthly_date(self):
-    #     self.ensure_one()
-    #     if not self.month:
-    #         return False
-    #     plan_year = self.tuition_plan_id.plan_year
-    #     if self.day_type == 'last_day':
-    #         day = 31
-    #     elif self.day_type == 'day_number':
-    #         day = int(self.day_of_the_month)
-    #     else:
-    #         day = 1
+    def _get_monthly_date(self):
+        self.ensure_one()
+        if not self.month:
+            return False
+        plan_year = self.tuition_plan_id.plan_year
+        if self.day_type == 'last_day':
+            day = 31
+        elif self.day_type == 'day_number':
+            day = int(self.day_of_the_month)
+        else:
+            day = 1
 
-    #     # raise UserError(str(plan_year))
-    #     next_date = datetime.date(year=plan_year, month=1, day=1) + relativedelta(month=int(self.month), day=day)
-    #     next_date = self.move_next_year_if_needed(next_date)
-    #     return next_date
+        
+        next_date = datetime.date(year=plan_year, month=1, day=1) + relativedelta(month=int(self.month), day=day)
+        next_date = self.move_next_year_if_needed(next_date)
+
+        raise UserError([str(plan_year),next_date])
+        return next_date
 
     def get_previous_installment(self):
         sorted_sibling_installments = self.tuition_plan_id.installment_ids.sorted('sequence')
@@ -1239,20 +1241,20 @@ class TuitionTemplateInstallment(models.Model):
                 } for installment in self]
         return values
 
-    def _get_monthly_date(self):
-        self.ensure_one()
-        if not self.month:
-            return False
-        today = fields.Date.today()
-        if self.day_type == 'last_day':
-            day = 31
-        elif self.day_type == 'day_number':
-            day = int(self.day_of_the_month)
-        else:
-            day = 1
-        next_date = today + relativedelta(month=int(self.month), day=day)
-        next_date = self.move_next_year_if_needed(next_date)
-        return next_date
+    # def _get_monthly_date(self):
+    #     self.ensure_one()
+    #     if not self.month:
+    #         return False
+    #     today = fields.Date.today()
+    #     if self.day_type == 'last_day':
+    #         day = 31
+    #     elif self.day_type == 'day_number':
+    #         day = int(self.day_of_the_month)
+    #     else:
+    #         day = 1
+    #     next_date = today + relativedelta(month=int(self.month), day=day)
+    #     next_date = self.move_next_year_if_needed(next_date)
+    #     return next_date
 
     def get_previous_installment(self):
         sorted_sibling_installments = self.tuition_template_id.installment_ids.sorted('sequence')
