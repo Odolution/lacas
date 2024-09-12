a00b15e8718f9eded489bf549ab5351cc4ac2c9d
Muhammad Huzaifa Shaikh <57531626+Huzaifa552@users.noreply.github.com>
2024-09-12 15:34:05 +0500

    Comment out _get_monthly_date and changes in another _get_monthly_date

--- ol_school_account/models/ol_finance_model.py
+++ ol_school_account/models/ol_finance_model.py
@@ -6,7 +6,7 @@ from odoo.fields import Command
 
 import datetime
 from dateutil.relativedelta import relativedelta
-
+from datetime import date
 from odoo.tools import float_is_zero, html_keep_url, is_html_empty
 
 import json
@@ -416,7 +416,7 @@ class TuitionPlanInstallment(models.Model):
             day = int(self.day_of_the_month)
         else:
             day = 1
-        next_date = datetime.date(year=plan_year, month=1, day=1) + relativedelta(month=int(self.month), day=day)
+        next_date = date(year=plan_year, month=1, day=1) + relativedelta(month=int(self.month), day=day)
         next_date = self.move_next_year_if_needed(next_date)
         return next_date
 
@@ -1236,20 +1236,21 @@ class TuitionTemplateInstallment(models.Model):
                 } for installment in self]
         return values
 
-    def _get_monthly_date(self):
-        self.ensure_one()
-        if not self.month:
-            return False
-        today = fields.Date.today()
-        if self.day_type == 'last_day':
-            day = 31
-        elif self.day_type == 'day_number':
-            day = int(self.day_of_the_month)
-        else:
-            day = 1
-        next_date = today + relativedelta(month=int(self.month), day=day)
-        next_date = self.move_next_year_if_needed(next_date)
-        return next_date
+    # Comment By Huzaifa
+    # def _get_monthly_date(self):
+    #     self.ensure_one()
+    #     if not self.month:
+    #         return False
+    #     today = fields.Date.today()
+    #     if self.day_type == 'last_day':
+    #         day = 31
+    #     elif self.day_type == 'day_number':
+    #         day = int(self.day_of_the_month)
+    #     else:
+    #         day = 1
+    #     next_date = today + relativedelta(month=int(self.month), day=day)
+    #     next_date = self.move_next_year_if_needed(next_date)
+    #     return next_date
 
     def get_previous_installment(self):
         sorted_sibling_installments = self.tuition_template_id.installment_ids.sorted('sequence')
