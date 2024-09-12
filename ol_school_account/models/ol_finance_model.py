ec8fa6c29f53e6a72e076d5e779eac8523cde61c
Muhammad Huzaifa Shaikh <57531626+Huzaifa552@users.noreply.github.com>
2024-09-12 13:14:54 +0500

    comment out _get_monthly_date

--- ol_school_account/models/ol_finance_model.py
+++ ol_school_account/models/ol_finance_model.py
@@ -405,20 +405,22 @@ class TuitionPlanInstallment(models.Model):
         elif records._name == 'account.move':
             records.sudo().write({'is_move_sent': True})
 
-    def _get_monthly_date(self):
-        self.ensure_one()
-        if not self.month:
-            return False
-        plan_year = self.tuition_plan_id.plan_year
-        if self.day_type == 'last_day':
-            day = 31
-        elif self.day_type == 'day_number':
-            day = int(self.day_of_the_month)
-        else:
-            day = 1
-        next_date = datetime.date(year=plan_year, month=1, day=1) + relativedelta(month=int(self.month), day=day)
-        next_date = self.move_next_year_if_needed(next_date)
-        return next_date
+    # def _get_monthly_date(self):
+    #     self.ensure_one()
+    #     if not self.month:
+    #         return False
+    #     plan_year = self.tuition_plan_id.plan_year
+    #     if self.day_type == 'last_day':
+    #         day = 31
+    #     elif self.day_type == 'day_number':
+    #         day = int(self.day_of_the_month)
+    #     else:
+    #         day = 1
+
+    #     # raise UserError(str(plan_year))
+    #     next_date = datetime.date(year=plan_year, month=1, day=1) + relativedelta(month=int(self.month), day=day)
+    #     next_date = self.move_next_year_if_needed(next_date)
+    #     return next_date
 
     def get_previous_installment(self):
         sorted_sibling_installments = self.tuition_plan_id.installment_ids.sorted('sequence')
