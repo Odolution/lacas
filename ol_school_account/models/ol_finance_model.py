123a96c90adff9a4b8cbf7a1538718ea4a696156
Odolution <104791801+Odolution@users.noreply.github.com>
2024-09-14 21:29:58 +0500

    Update ol_finance_model.py

--- ol_school_account/models/ol_finance_model.py
+++ ol_school_account/models/ol_finance_model.py
@@ -920,8 +920,8 @@ class TuitionPlan(models.Model):
             # discount_to_add = []
             product_in_line = []
             installment_obj = [i for i in rec.line_ids[-1].installment_ids]
-            raise UserError(str(installment_obj))
-            
+            raise UserError(str(installment_obj[0].name))
+
             for line in rec.line_ids:
                 product_in_line.append(line.product_id.id)
     
