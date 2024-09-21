af609bafaa0a2ba8ff26979d1fe9f78a3f392770
Odolution <104791801+Odolution@users.noreply.github.com>
2024-09-21 20:26:14 +0500

    Update ol_finance_model.py

--- ol_school_account/models/ol_finance_model.py
+++ ol_school_account/models/ol_finance_model.py
@@ -469,7 +469,7 @@ class ConcessionLine(models.Model):
 
         for line in self:
             lst = []
-            raise UserError(str(installment_obj))
+            raise UserError(str(installment_obj[0].id))
             for j in installment_obj:
                 # raise UserError(j.ids)
                 lst.append(j.ids)
