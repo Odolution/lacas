ed9e53b83c6b476bf754861a768204ac81c59fa5
Odolution <104791801+Odolution@users.noreply.github.com>
2024-09-21 19:46:10 +0500

    Update ol_finance_model.py

--- ol_school_account/models/ol_finance_model.py
+++ ol_school_account/models/ol_finance_model.py
@@ -1374,6 +1374,7 @@ class TuitionPlan(models.Model):
     def update_line_concession(self):
         for plan in self:
             installment_obj = [plan.line_ids[-1].mapped('installment_ids')]
+            raise UserError(installment_obj)
 
             line_concession_list = plan.student_id.concession_line_ids.get_concession_values(installment_obj)
             line_concession_list = [Command.create(vals) for vals in line_concession_list]
