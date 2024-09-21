a62a3f8fe0f55402adc3380d2e579d460fa168ae
Odolution-developer <odolutiondeveloper@gmail.com>
2024-09-21 14:15:48 +0500

    Update create_admission_challan.py

--- admission_challan/wizard/create_admission_challan.py
+++ admission_challan/wizard/create_admission_challan.py
@@ -6,15 +6,23 @@ class AdmissionChallanWizard(models.TransientModel):
 
     template_id = fields.Many2one('tuition.template', string="Fee Template", required=True)
     student_id = fields.Many2one('school.student', string="Student", required=True)
+    student_ids = fields.Many2one('school.student', string="Student")
     
     def action_create_tuition_plan(self):
         self.ensure_one()
         if self.template_id:
-            # Call the function on the template_id (tuition.template)
-            self.env['tuition.plan'].create({
-            'student_id': self.student_id.id,
-            'tuition_template_id': self.template_id.id,
-        })
+            if self.student_ids:
+                for sid in self.student_ids:                    
+                    self.env['tuition.plan'].create({
+                    'student_id': sid.id,
+                    'tuition_template_id': self.template_id.id,
+                })
+            else:
+                # Call the function on the template_id (tuition.template)
+                self.env['tuition.plan'].create({
+                'student_id': self.student_id.id,
+                'tuition_template_id': self.template_id.id,
+            })
         return {'type': 'ir.actions.act_window_close'}
 
     def action_discard(self):
