872f5f33533d11cd9ae509b8be24505f7415bde3
Odolution-developer <odolutiondeveloper@gmail.com>
2024-09-21 12:14:37 +0500

    Update __manifest__.py

--- admission_challan/__manifest__.py
+++ admission_challan/__manifest__.py
@@ -7,11 +7,11 @@
     "description": "",
     "depends": ["base", "ol_school_account", "ol_school_manager"],
     "data": [
-            # "security/ir.model.access.csv",
-            # "data/server_action_multiple_admission_challan.xml",
-            # "views/admission_challan_view.xml" ,  
-            # "views/tuition_template_view.xml" ,  
-            # "wizard/create_admission_challan.xml",
+            "security/ir.model.access.csv",
+            "data/server_action_multiple_admission_challan.xml",
+            "views/admission_challan_view.xml" ,  
+            "views/tuition_template_view.xml" ,  
+            "wizard/create_admission_challan.xml",
             
     ],
     "demo": [],
