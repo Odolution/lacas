d005aa2675ff6b82d648fe03aa28ddebbd9e4d6b
Odolution-developer <143722225+odolution-developer@users.noreply.github.com>
2024-09-23 11:37:43 +0500

    Update tuition_template.py

--- admission_challan/models/tuition_template.py
+++ admission_challan/models/tuition_template.py
@@ -5,8 +5,7 @@ class TuitionTemplate(models.Model):
     _inherit = 'tuition.template'
     
     program_id = fields.Many2one('school.program', string='Program')
-    grade_level_ids = fields.Many2many('school.grade.level', string='Grade Levels',  
-    domain= [('program_id', '=', program_id.id)])
+    grade_level_ids = fields.Many2many('school.grade.level', string='Grade Levels',  domain= [('program_id', '=', program_id)])
 
     # @api.onchange('program_id')
     # def _onchange_program_id(self):
