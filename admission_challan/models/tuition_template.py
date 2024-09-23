3b94b0e4dbfcdfb7f7be71bd1ee9dc2ee49af2f4
Odolution-developer <143722225+odolution-developer@users.noreply.github.com>
2024-09-23 11:36:05 +0500

    .

--- admission_challan/models/tuition_template.py
+++ admission_challan/models/tuition_template.py
@@ -3,5 +3,23 @@ from odoo.exceptions import UserError
 
 class TuitionTemplate(models.Model):
     _inherit = 'tuition.template'
+    
+    program_id = fields.Many2one('school.program', string='Program')
+    grade_level_ids = fields.Many2many('school.grade.level', string='Grade Levels',  
+    domain= [('program_id', '=', program_id.id)])
 
-    grade_level_ids = fields.Many2many('school.grade.level', string='Grade Levels')
+    # @api.onchange('program_id')
+    # def _onchange_program_id(self):
+    #     if self.program_id:
+    #         return {
+    #             'domain': {
+    #                 'grade_level_ids': [('program_id', '=', self.program_id.id)]
+    #             }
+    #         }
+    #     else:
+    #         return {
+    #             'domain': {
+    #                 'grade_level_ids': []
+    #             }
+    #         }
+
