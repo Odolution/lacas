43f723ffc7e8639b9a0353d2311a35d53e3e8f19
Odolution <104791801+Odolution@users.noreply.github.com>
2024-09-28 19:19:48 +0500

    Update ol_inherit_model.py

--- ol_school_account/models/ol_inherit/ol_inherit_model.py
+++ ol_school_account/models/ol_inherit/ol_inherit_model.py
@@ -205,6 +205,8 @@ class Student(models.Model):
 
         for plan in self.tuition_plan_ids:
 
+            raise ValidationError(str(plan.line_ids[-1].installment_ids))
+
             installment_obj = plan.line_ids[-1].installment_ids
             line_concession_list = plan.student_id.concession_line_ids.get_concession_values(installment_obj)
             line_concession_list = [Command.create(vals) for vals in line_concession_list]
