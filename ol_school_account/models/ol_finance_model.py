9c95334b2e70e7d8b209b186ef92ce7765c184e5
Odolution <104791801+Odolution@users.noreply.github.com>
2024-09-28 16:16:46 +0500

    Update ol_finance_model.py

--- ol_school_account/models/ol_finance_model.py
+++ ol_school_account/models/ol_finance_model.py
@@ -1377,8 +1377,9 @@ class TuitionPlan(models.Model):
             if force or plan._is_tuition_pending_to_create_charge():
                 
                 other_plans = plan._find_other_plans_in_same_group()
-                raise UserError('hit')
+                
                 next_installments = other_plans.mapped('next_installment_id')
+                raise UserError('hitt')
                 if next_installments:
                     next_installments.create_recurring_charge(origin_plan=plan)
 
