abc0c06ecd0c7eabec63f27080789c3365f54ef2
Odolution-developer <143722225+odolution-developer@users.noreply.github.com>
2024-10-02 17:56:19 +0500

    api_billing

--- api_billing/models/main.py
+++ api_billing/models/main.py
@@ -128,8 +128,8 @@ class Billing(http.Controller):
 
 
             if not move_ids:
-                return Response(json.dumps({'move_id':move_ids,'voucher_no':voucher_id}), content_type="application/json", status=200)
-            
+                # return Response(json.dumps({'move_id':move_ids,'voucher_no':voucher_id}), content_type="application/json", status=200)
+                return {'move_id':str(move_ids),'voucher_no':str(voucher_id)}
                 return Response(json.dumps({
                     "Status": {
                         "StatusCode": 5000,
